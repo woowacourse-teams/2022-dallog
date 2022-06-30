e43b8700c4b95eea2baf54c91d5ff208df40be05
summerlunaa <summerlunaa9@gmail.com>
2022-06-30 17:44:34 +0900

    feat: 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/allog/dallog/DallogApplication.java
@@ -0,0 +1,13 @@
+package com.allog.dallog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DallogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DallogApplication.class, args);
+	}
+
+}
