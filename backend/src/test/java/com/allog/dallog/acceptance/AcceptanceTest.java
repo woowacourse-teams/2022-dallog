64e3e42c59bb14c705278b179ee1247966cb6abb
koo <gudonghee2000@gmail.com>
2022-07-04 19:24:56 +0900

    test: Acceptance테스트 설정 객체 추가

---
+++ backend/src/test/java/com/allog/dallog/acceptance/AcceptanceTest.java
@@ -0,0 +1,20 @@
+package com.allog.dallog.acceptance;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+}
