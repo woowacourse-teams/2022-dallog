1587d112fcb39f5f47816789dcbe7e9b6e9722cf
jhy979 <32920566+jhy979@users.noreply.github.com>
2022-07-05 22:14:34 +0900

    refactor: 버튼 공통 컴포넌트 스토리 title 수정

--- frontend/src/components/@common/Button.stories.tsx
+++ frontend/src/components/@common/Button.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
 
 export default {
-  title: 'Components/Button',
+  title: 'Components/@Common/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
