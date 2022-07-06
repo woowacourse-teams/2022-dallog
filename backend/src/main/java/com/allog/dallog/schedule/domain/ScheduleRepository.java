f8215acb4ebcbd5bb600c21a4a6175b23721e97b
hyeonic <dev.hyeonic@gmail.com>
2022-07-06 15:03:41 +0900

    feat: 시작일시와 종료일시 사이에 해당하는 일정 조회 기능 추가

--- backend/src/main/java/com/allog/dallog/schedule/domain/ScheduleRepository.java
+++ backend/src/main/java/com/allog/dallog/schedule/domain/ScheduleRepository.java
@@ -1,7 +1,14 @@
 package com.allog.dallog.schedule.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
+    @Query("SELECT s "
+        + "FROM Schedule s "
+        + "WHERE s.period.startDateTime <= :endDate AND s.period.endDateTime >= :startDate")
+    List<Schedule> findByBetween(final LocalDateTime startDate, final LocalDateTime endDate);
 }
