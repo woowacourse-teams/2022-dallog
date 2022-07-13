77fb4968981c990571f1c26600af0626e157b55a
summerlunaa <summerlunaa9@gmail.com>
2022-07-13 15:13:36 +0900

    feat: CategoryRepository 구현

---
+++ backend/src/main/java/com/allog/dallog/category/domain/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.allog.dallog.category.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
