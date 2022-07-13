fa135a26e7af0ba814494fe8e45951e96f0eb272
summerlunaa <summerlunaa9@gmail.com>
2022-07-13 17:15:02 +0900

    test: 로직 메서드로 분리 및 이동

--- backend/src/test/java/com/allog/dallog/acceptance/CategoryAcceptanceTest.java
+++ backend/src/test/java/com/allog/dallog/acceptance/CategoryAcceptanceTest.java
@@ -1,14 +1,11 @@
 package com.allog.dallog.acceptance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.allog.dallog.category.dto.request.CategoryCreateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @DisplayName("카테고리 관련 기능")
@@ -19,17 +16,21 @@ public class CategoryAcceptanceTest extends AcceptanceTest {
     void 정상적인_카테고리_정보를_등록하면_상태코드_201을_반환한다() {
         // given
         String name = "BE 공식일정";
-        CategoryCreateRequest request = new CategoryCreateRequest(name);
 
         // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        ExtractableResponse<Response> response = 새로운_카테고리를_등록한다(name);
+
+        // then
+        상태코드_201이_반환된다(response);
+    }
+
+    private ExtractableResponse<Response> 새로운_카테고리를_등록한다(final String name) {
+        CategoryCreateRequest request = new CategoryCreateRequest(name);
+        return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(request)
                 .when().post("/api/categories")
                 .then().log().all()
                 .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 }
