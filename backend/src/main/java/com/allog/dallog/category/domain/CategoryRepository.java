e2a0ed6015a5bf9bfa4328f6d97a570e8d444640
summerlunaa <summerlunaa9@gmail.com>
2022-07-14 14:00:02 +0900

    feat: CategoryRepository에 슬라이스를 통한 카테고리 조회 기능 구현

--- backend/src/main/java/com/allog/dallog/category/domain/CategoryRepository.java
+++ backend/src/main/java/com/allog/dallog/category/domain/CategoryRepository.java
@@ -1,6 +1,10 @@
 package com.allog.dallog.category.domain;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Slice<Category> findSliceBy(final Pageable pageable);
 }
