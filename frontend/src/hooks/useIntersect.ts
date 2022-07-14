627c6e125d87a646179bb394e3ef51eb275d55da
jhy979 <jhy979@gmail.com>
2022-07-14 17:25:56 +0900

    feat: intersect 커스텀훅 구현

---
+++ frontend/src/hooks/useIntersect.ts
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+type IntersectHandler = (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void;
+
+function useIntersect(onIntersect: IntersectHandler, options?: IntersectionObserverInit) {
+  const ref = useRef<HTMLDivElement>(null);
+  const callback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) onIntersect(entry, observer);
+      });
+    },
+    [onIntersect]
+  );
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, options, callback]);
+
+  return ref;
+}
+
+export default useIntersect;
