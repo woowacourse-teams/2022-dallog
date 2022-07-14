870ee7395dc97ceeb08232a15bebcb9d359dd451
summerlunaa <summerlunaa9@gmail.com>
2022-07-14 16:12:07 +0900

    feat: JPA auditing 적용
    - BaseEntity에 생성일시와 수정일시 구현
    - 다른 Entity가 상속받도록 수정
    - test 추가

--- backend/src/main/java/com/allog/dallog/DallogApplication.java
+++ backend/src/main/java/com/allog/dallog/DallogApplication.java
@@ -2,7 +2,9 @@ package com.allog.dallog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DallogApplication {
 
