dbc4ce9c4f9a73432604abd80298cbd6b7eff6c1
summerlunaa <summerlunaa9@gmail.com>
2022-07-14 14:34:56 +0900

    feat: 카테고리 조회 api 구현

--- backend/src/main/java/com/allog/dallog/category/controller/CategoryController.java
+++ backend/src/main/java/com/allog/dallog/category/controller/CategoryController.java
@@ -1,13 +1,19 @@
 package com.allog.dallog.category.controller;
 
 import com.allog.dallog.category.dto.request.CategoryCreateRequest;
+import com.allog.dallog.category.dto.response.CategoryResponse;
 import com.allog.dallog.category.service.CategoryService;
+import com.allog.dallog.global.dto.ListResponse;
 import java.net.URI;
+import java.util.List;
 import javax.validation.Valid;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/categories")
@@ -24,5 +30,13 @@ public class CategoryController {
     public ResponseEntity<Void> save(@Valid @RequestBody final CategoryCreateRequest request) {
         Long id = categoryService.save(request);
         return ResponseEntity.created(URI.create("/api/categories/" + id)).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<ListResponse<CategoryResponse>> findSliceBy(@RequestParam final int page,
+                                                                      @RequestParam final int size) {
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        List<CategoryResponse> responses = categoryService.findSliceBy(pageRequest);
+        return ResponseEntity.ok(new ListResponse<>(responses));
     }
 }
