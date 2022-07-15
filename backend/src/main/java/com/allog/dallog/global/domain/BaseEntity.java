f22af63d4ce24692b936c6c5de8d531dd3557d07
summerlunaa <summerlunaa9@gmail.com>
2022-07-15 15:46:35 +0900

    fix: 어노테이션 수정

--- backend/src/main/java/com/allog/dallog/global/domain/BaseEntity.java
+++ backend/src/main/java/com/allog/dallog/global/domain/BaseEntity.java
@@ -13,11 +13,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "created_at", updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "updated_at", updatable = false)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     public LocalDateTime getCreatedAt() {
