e180f4bbbdbef0bb0608ca6508b11ac70fa4148e
summerlunaa <summerlunaa9@gmail.com>
2022-07-15 15:47:59 +0900

    refactor: JPA 설정 파일 분리

--- backend/src/main/java/com/allog/dallog/DallogApplication.java
+++ backend/src/main/java/com/allog/dallog/DallogApplication.java
@@ -2,9 +2,7 @@ package com.allog.dallog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class DallogApplication {
 
---
+++ backend/src/main/java/com/allog/dallog/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.allog.dallog.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
