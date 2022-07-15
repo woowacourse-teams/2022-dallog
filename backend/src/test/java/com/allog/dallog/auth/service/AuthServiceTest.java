70e8faf5d75d582e2af806cb2d97835564ca1e9b
devHudi <devhudi@gmail.com>
2022-07-15 15:15:51 +0900

    test: AuthService의 회원 생성 관련 테스트 추가

--- backend/src/test/java/com/allog/dallog/auth/service/AuthServiceTest.java
+++ backend/src/test/java/com/allog/dallog/auth/service/AuthServiceTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.allog.dallog.auth.dto.TokenResponse;
 import com.allog.dallog.config.TestConfig;
+import com.allog.dallog.member.domain.Member;
+import com.allog.dallog.member.domain.MemberRepository;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,9 @@ class AuthServiceTest {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @DisplayName("구글 로그인을 위한 링크를 생성한다.")
     @Test
@@ -36,5 +42,37 @@ class AuthServiceTest {
 
         // then
         assertThat(actual.getAccessToken()).isNotEmpty();
+    }
+
+    @DisplayName("Authorization Code를 받으면 회원이 데이터베이스에 저장된다.")
+    @Test
+    void Authorization_Code를_받으면_회원이_데이터베이스에_저장된다() {
+        // given
+        String code = "authorization code";
+        authService.generateTokenWithCode(code);
+
+        // when
+        boolean actual = memberRepository.existsByEmail("dev.hyeonic@gmail.com");
+        // SutbOAuthClient가 반환하는 OAuthMember의 이메일
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @DisplayName("이미 가입된 회원에 대한 Authorization Code를 전달받으면 추가로 유저가 생성되지 않는다")
+    @Test
+    void 이미_가입된_회원에_대한_Authorization_Code를_전달받으면_유저가_추가로_생성되지_않는다() {
+        // 이미 가입된 유저가 소셜 로그인 버튼을 클릭했을 경우엔 회원가입 과정이 생략되고, 곧바로 access token이 발급되어야 한다.
+
+        // given
+        String code = "authorization code";
+        authService.generateTokenWithCode(code);
+
+        // when & then
+        authService.generateTokenWithCode(code);
+        List<Member> actual = memberRepository.findAll();
+
+        // then
+        assertThat(actual).hasSize(1);
     }
 }
