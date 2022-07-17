b58196cee25f2ce75f3d73bf6d11197f30b76625
devHudi <devhudi@gmail.com>
2022-07-18 01:41:42 +0900

    refactor: RestController와 RequestMapping 어노테이션의 순서 변경

--- backend/src/main/java/com/allog/dallog/auth/controller/AuthController.java
+++ backend/src/main/java/com/allog/dallog/auth/controller/AuthController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/api/auth")
+@RestController
 public class AuthController {
 
     private final AuthService authService;
