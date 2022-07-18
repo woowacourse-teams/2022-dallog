e423f9959e332cb1990e83fd77ee406eb097c0d6
hyeonic <dev.hyeonic@gmail.com>
2022-07-18 21:23:44 +0900

    test: WebMvcTest에 필요한 실 객체 JwtTokenProvier 주입

--- backend/src/test/java/com/allog/dallog/schedule/controller/ScheduleControllerTest.java
+++ backend/src/test/java/com/allog/dallog/schedule/controller/ScheduleControllerTest.java
@@ -12,6 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.allog.dallog.auth.support.JwtTokenProvider;
 import com.allog.dallog.schedule.dto.request.ScheduleCreateRequest;
 import com.allog.dallog.schedule.dto.response.ScheduleResponse;
 import com.allog.dallog.schedule.service.ScheduleService;
@@ -26,11 +27,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureRestDocs
 @WebMvcTest(ScheduleController.class)
+@Import(JwtTokenProvider.class)
 class ScheduleControllerTest {
 
     @Autowired
