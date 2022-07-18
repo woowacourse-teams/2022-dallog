11d0a6958022fb26714c33ff3b3b28fb0e10cec0
koo <gudonghee2000@gmail.com>
2022-07-18 20:32:22 +0900

    fix: 테스트 격리 문제 해결

--- backend/src/test/java/com/allog/dallog/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/allog/dallog/acceptance/AcceptanceTest.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.annotation.DirtiesContext;
 
-@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AcceptanceTest {
 
