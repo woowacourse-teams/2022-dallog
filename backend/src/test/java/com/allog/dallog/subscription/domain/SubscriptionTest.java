1519657e0651d6d018fd5db87900b98f6e7788aa
hyeonic <dev.hyeonic@gmail.com>
2022-07-19 00:41:18 +0900

    feat: 구독을 표현하기 위한 Subscription 추가 및 검증

---
+++ backend/src/test/java/com/allog/dallog/subscription/domain/SubscriptionTest.java
@@ -0,0 +1,42 @@
+package com.allog.dallog.subscription.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.allog.dallog.category.domain.Category;
+import com.allog.dallog.fixture.CategoryFixtures;
+import com.allog.dallog.fixture.MemberFixtures;
+import com.allog.dallog.member.domain.Member;
+import com.allog.dallog.subscription.exception.InvalidSubscriptionException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SubscriptionTest {
+
+    @DisplayName("구독을 생성한다.")
+    @Test
+    void 구독을_생성한다() {
+        // given
+        Member member = MemberFixtures.MEMBER;
+        Category category = CategoryFixtures.CATEGORY;
+        String color = "#c9ad2e";
+
+        // when & then
+        assertDoesNotThrow(() -> new Subscription(member, category, color));
+    }
+
+    @DisplayName("색 정보 형식이 잘못된 경우 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"#111", "#1111", "#11111", "123456", "#**1234", "##12345", "334172#"})
+    void 색_정보_형식이_잘못된_경우_예외를_던진다(final String color) {
+        // given
+        Member member = MemberFixtures.MEMBER;
+        Category category = CategoryFixtures.CATEGORY;
+
+        // when & then
+        assertThatThrownBy(() -> new Subscription(member, category, color))
+                .isInstanceOf(InvalidSubscriptionException.class);
+    }
+}
