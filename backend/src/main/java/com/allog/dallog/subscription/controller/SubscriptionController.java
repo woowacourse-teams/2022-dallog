5bdf699c1d51ee26ab14b0b9c9501146e3fe2b6b
hyeonic <dev.hyeonic@gmail.com>
2022-07-20 00:11:16 +0900

    test: subscription 인수 테스트 추가

---
+++ backend/src/main/java/com/allog/dallog/subscription/controller/SubscriptionController.java
@@ -0,0 +1,33 @@
+package com.allog.dallog.subscription.controller;
+
+import com.allog.dallog.auth.dto.LoginMember;
+import com.allog.dallog.auth.support.AuthenticationPrincipal;
+import com.allog.dallog.subscription.dto.request.SubscriptionCreateRequest;
+import com.allog.dallog.subscription.dto.response.SubscriptionResponse;
+import com.allog.dallog.subscription.service.SubscriptionService;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubscriptionController {
+
+    private final SubscriptionService subscriptionService;
+
+    public SubscriptionController(final SubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    @PostMapping("/api/members/me/categories/{categoryId}/subscriptions")
+    public ResponseEntity<SubscriptionResponse> save(@AuthenticationPrincipal final LoginMember loginMember,
+                                                     @PathVariable final Long categoryId,
+                                                     @RequestBody final SubscriptionCreateRequest request) {
+        Long id = subscriptionService.save(loginMember.getId(), categoryId, request);
+        SubscriptionResponse subscriptionResponse = subscriptionService.findById(id);
+        return ResponseEntity.created(
+                URI.create("/api/members/me/" + categoryId + "/subscriptions/" + id)).body(subscriptionResponse);
+    }
+}
---
+++ backend/src/test/java/com/allog/dallog/common/fixtures/SubscriptionFixtures.java
@@ -0,0 +1,6 @@
+package com.allog.dallog.fixture;
+
+public class SubscriptionFixtures {
+
+    public static final String COLOR = "#ffffff";
+}
