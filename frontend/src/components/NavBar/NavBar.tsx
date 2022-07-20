94b1307568c097360aee779e5c6df1dba1b57c9c
Daye Lee <508yeah@gmail.com>
2022-07-20 17:34:10 +0900

    feat: 내비게이션 바 메뉴 클릭 시 대응하는 페이지로 이동

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,12 @@
 import { useTheme } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 
 import { userState } from '@/atoms';
 
 import Button from '@/components/@common/Button/Button';
+
+import { PATH } from '@/constants';
 
 import { BiCategory } from 'react-icons/bi';
 import { FaUserCircle } from 'react-icons/fa';
@@ -17,10 +20,25 @@ interface NavBarProps {
 function NavBar({ openLoginModal }: NavBarProps) {
   const [user] = useRecoilState(userState);
   const theme = useTheme();
+  const navigate = useNavigate();
+
+  const handleClickLogoButton = () => {
+    navigate(PATH.MAIN);
+  };
+
+  const handleClickCategoryMenuButton = () => {
+    navigate(PATH.CATEGORY);
+  };
+
+  const handleClickProfileMenuButton = () => {
+    navigate(PATH.PROFILE);
+  };
 
   return (
     <div css={navBar}>
-      <Button cssProp={menu(theme)}>🌙&nbsp;&nbsp;달록</Button>
+      <Button cssProp={menu(theme)} onClick={handleClickLogoButton}>
+        🌙&nbsp;&nbsp;달록
+      </Button>
       <div css={menus}>
         {!user.accessToken && (
           <Button cssProp={loginButton(theme)} onClick={openLoginModal}>
@@ -29,11 +47,11 @@ function NavBar({ openLoginModal }: NavBarProps) {
         )}
         {user.accessToken && (
           <>
-            <Button cssProp={menu(theme)} onClick={openLoginModal}>
+            <Button cssProp={menu(theme)} onClick={handleClickCategoryMenuButton}>
               <BiCategory size={28} />
               <span css={menuTitle}>카테고리</span>
             </Button>
-            <Button cssProp={menu(theme)} onClick={openLoginModal}>
+            <Button cssProp={menu(theme)} onClick={handleClickProfileMenuButton}>
               <FaUserCircle size={28} />
               <span css={menuTitle}>프로필</span>
             </Button>
