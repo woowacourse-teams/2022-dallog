e532d61ea3f8b974e8fd73c8b51122b9b118d8ce
Daye Lee <508yeah@gmail.com>
2022-07-20 17:29:28 +0900

    feat: 로그인 상태에 따라 내비게이션 바 메뉴 변경

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,21 +1,45 @@
 import { useTheme } from '@emotion/react';
+import { useRecoilState } from 'recoil';
+
+import { userState } from '@/atoms';
 
 import Button from '@/components/@common/Button/Button';
 
-import { loginButton, navBar } from './NavBar.styles';
+import { BiCategory } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
+
+import { loginButton, menu, menus, menuTitle, navBar } from './NavBar.styles';
 
 interface NavBarProps {
   openLoginModal?: () => void;
 }
 
 function NavBar({ openLoginModal }: NavBarProps) {
+  const [user] = useRecoilState(userState);
   const theme = useTheme();
 
   return (
     <div css={navBar}>
-      <Button cssProp={loginButton(theme)} onClick={openLoginModal}>
-        👤&nbsp;&nbsp;로그인
-      </Button>
+      <Button cssProp={menu(theme)}>🌙&nbsp;&nbsp;달록</Button>
+      <div css={menus}>
+        {!user.accessToken && (
+          <Button cssProp={loginButton(theme)} onClick={openLoginModal}>
+            로그인
+          </Button>
+        )}
+        {user.accessToken && (
+          <>
+            <Button cssProp={menu(theme)} onClick={openLoginModal}>
+              <BiCategory size={28} />
+              <span css={menuTitle}>카테고리</span>
+            </Button>
+            <Button cssProp={menu(theme)} onClick={openLoginModal}>
+              <FaUserCircle size={28} />
+              <span css={menuTitle}>프로필</span>
+            </Button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
