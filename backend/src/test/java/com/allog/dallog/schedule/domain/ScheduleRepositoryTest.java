3071b8c057e0cabcdd714df4b429f6a4e8806dd7
koo <gudonghee2000@gmail.com>
2022-07-21 21:39:39 +0900

    refactor: Schedule의 테스트코드 변수명 변경

--- backend/src/test/java/com/allog/dallog/schedule/domain/ScheduleRepositoryTest.java
+++ backend/src/test/java/com/allog/dallog/schedule/domain/ScheduleRepositoryTest.java
@@ -23,14 +23,14 @@ class ScheduleRepositoryTest {
     @Test
     void 시작일시와_종료일시를_전달하면_그_사이에_해당하는_일정을_조회한다() {
         // given
-        Schedule schedule1 = new Schedule(TITLE, LocalDateTime.of(2022, 7, 14, 14, 20),
+        Schedule today = new Schedule(TITLE, LocalDateTime.of(2022, 7, 14, 14, 20),
                 LocalDateTime.of(2022, 7, 15, 16, 20), MEMO);
 
-        Schedule schedule2 = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
+        Schedule nextMonth = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
                 LocalDateTime.of(2022, 8, 15, 16, 20), MEMO);
 
-        scheduleRepository.save(schedule1);
-        scheduleRepository.save(schedule2);
+        scheduleRepository.save(today);
+        scheduleRepository.save(nextMonth);
 
         // when
         List<Schedule> schedules = scheduleRepository.findByBetween(START_DAY_OF_MONTH, END_DAY_OF_MONTH);
@@ -43,14 +43,14 @@ class ScheduleRepositoryTest {
     @Test
     void 시작일시와_종료일시를_전달할_때_일정의_시작날짜가_시작일시와_같으면_조회된다() {
         // given
-        Schedule schedule1 = new Schedule(TITLE, LocalDateTime.of(2022, 7, 1, 0, 0),
+        Schedule startDayOfMonth = new Schedule(TITLE, LocalDateTime.of(2022, 7, 1, 0, 0),
                 LocalDateTime.of(2022, 7, 15, 16, 20), MEMO);
 
-        Schedule schedule2 = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
+        Schedule nextMonth = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
                 LocalDateTime.of(2022, 8, 15, 16, 20), MEMO);
 
-        scheduleRepository.save(schedule1);
-        scheduleRepository.save(schedule2);
+        scheduleRepository.save(startDayOfMonth);
+        scheduleRepository.save(nextMonth);
 
         // when
         List<Schedule> schedules = scheduleRepository.findByBetween(START_DAY_OF_MONTH, END_DAY_OF_MONTH);
@@ -63,14 +63,14 @@ class ScheduleRepositoryTest {
     @Test
     void 시작일시와_종료일시를_전달할_때_일정의_시작날짜가_종료일시와_같으면_조회된다() {
         // given
-        Schedule schedule1 = new Schedule(TITLE, START_DAY_OF_MONTH,
+        Schedule endDayOfMonth = new Schedule(TITLE, START_DAY_OF_MONTH,
                 LocalDateTime.of(2022, 8, 12, 13, 10), MEMO);
 
-        Schedule schedule2 = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
+        Schedule nextMonth = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
                 LocalDateTime.of(2022, 8, 15, 16, 20), MEMO);
 
-        scheduleRepository.save(schedule1);
-        scheduleRepository.save(schedule2);
+        scheduleRepository.save(endDayOfMonth);
+        scheduleRepository.save(nextMonth);
 
         // when
         List<Schedule> schedules = scheduleRepository.findByBetween(START_DAY_OF_MONTH, END_DAY_OF_MONTH);
@@ -83,14 +83,14 @@ class ScheduleRepositoryTest {
     @Test
     void 시작일시와_종료일시를_전달할_때_일정의_시작날짜가_종료일시_이후이면_조회되지_않는다() {
         // given
-        Schedule schedule1 = new Schedule(TITLE, LocalDateTime.of(2022, 7, 31, 0, 1),
+        Schedule afterDayOfMonth = new Schedule(TITLE, LocalDateTime.of(2022, 7, 31, 0, 1),
                 LocalDateTime.of(2022, 8, 12, 13, 10), MEMO);
 
-        Schedule schedule2 = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
+        Schedule nextMonth = new Schedule(TITLE, LocalDateTime.of(2022, 8, 15, 14, 20),
                 LocalDateTime.of(2022, 8, 15, 16, 20), MEMO);
 
-        scheduleRepository.save(schedule1);
-        scheduleRepository.save(schedule2);
+        scheduleRepository.save(afterDayOfMonth);
+        scheduleRepository.save(nextMonth);
 
         // when
         List<Schedule> schedules = scheduleRepository.findByBetween(START_DAY_OF_MONTH, END_DAY_OF_MONTH);
