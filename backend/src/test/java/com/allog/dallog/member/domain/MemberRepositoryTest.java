4ccc6fe4f15731dcdd9f4a0264e21c6eea2bff6f
summerlunaa <summerlunaa9@gmail.com>
2022-07-21 18:07:05 +0900

    test: Import 어노테이션 추가

--- backend/src/test/java/com/allog/dallog/member/domain/MemberRepositoryTest.java
+++ backend/src/test/java/com/allog/dallog/member/domain/MemberRepositoryTest.java
@@ -5,12 +5,15 @@ import static com.allog.dallog.common.fixtures.MemberFixtures.EMAIL;
 import static com.allog.dallog.common.fixtures.MemberFixtures.PROFILE_IMAGE_URI;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.allog.dallog.global.config.JpaConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@Import(JpaConfig.class)
 class MemberRepositoryTest {
 
     @Autowired
