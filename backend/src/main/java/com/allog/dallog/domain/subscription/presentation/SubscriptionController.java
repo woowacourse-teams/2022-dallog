e3ebce7fdbba8b377501b0ff9bcd85aa480df265
hyeonic <dev.hyeonic@gmail.com>
2022-07-27 13:16:58 +0900

    chore: subscription Controller 공통 path 추출

--- backend/src/main/java/com/allog/dallog/domain/subscription/presentation/SubscriptionController.java
+++ backend/src/main/java/com/allog/dallog/domain/subscription/presentation/SubscriptionController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/members/me")
 @RestController
 public class SubscriptionController {
 
@@ -24,7 +26,7 @@ public class SubscriptionController {
         this.subscriptionService = subscriptionService;
     }
 
-    @PostMapping("/api/members/me/categories/{categoryId}/subscriptions")
+    @PostMapping("/categories/{categoryId}/subscriptions")
     public ResponseEntity<SubscriptionResponse> save(@AuthenticationPrincipal final LoginMember loginMember,
                                                      @PathVariable final Long categoryId,
                                                      @RequestBody final SubscriptionCreateRequest request) {
@@ -33,13 +35,13 @@ public class SubscriptionController {
                 URI.create("/api/members/me/" + categoryId + "/subscriptions/" + response.getId())).body(response);
     }
 
-    @GetMapping("/api/members/me/subscriptions")
+    @GetMapping("/subscriptions")
     public ResponseEntity<SubscriptionsResponse> findMine(@AuthenticationPrincipal final LoginMember loginMember) {
         SubscriptionsResponse response = subscriptionService.findByMemberId(loginMember.getId());
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/api/members/me/subscriptions/{subscriptionId}")
+    @DeleteMapping("/subscriptions/{subscriptionId}")
     public ResponseEntity<Void> deleteById(@AuthenticationPrincipal final LoginMember loginMember,
                                            @PathVariable final Long subscriptionId) {
         subscriptionService.deleteByIdAndMemberId(subscriptionId, loginMember.getId());
