865c0624f2e4860cc4f17a5f210099a7137408e3
hyeonic <dev.hyeonic@gmail.com>
2022-07-28 20:24:54 +0900

    feat: truncate를 진행하기 위한 DatabaseCleanup 추가

--- backend/src/test/java/com/allog/dallog/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/allog/dallog/acceptance/AcceptanceTest.java
@@ -1,23 +1,27 @@
 package com.allog.dallog.acceptance;
 
+import com.allog.dallog.common.DatabaseCleanup;
 import com.allog.dallog.common.config.TestConfig;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/truncate.sql")
 @Import(TestConfig.class)
 class AcceptanceTest {
 
     @LocalServerPort
-    int port;
+    private int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
 
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        databaseCleanup.execute();
     }
 }
