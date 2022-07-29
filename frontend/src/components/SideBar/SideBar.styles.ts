53e5f7f41054298e592d72736fb7a156a39b9d6b
jhy979 <jhy979@gmail.com>
2022-07-29 12:36:00 +0900

    refactor: 불필요한 코드 제거

--- frontend/src/components/SideBar/SideBar.styles.ts
+++ frontend/src/components/SideBar/SideBar.styles.ts
@@ -18,38 +18,4 @@ const sideBar = ({ colors, flex }: Theme, isSideBarOpen: boolean) => css`
   transition: width 0.3s;
 `;
 
-const list = ({ flex }: Theme, isSideBarOpen: boolean) => css`
-  ${flex.column}
-
-  display: ${isSideBarOpen ? 'flex' : 'none'};
-  gap: 3rem;
-
-  width: 56rem;
-`;
-
-const title = ({ flex }: Theme) => css`
-  ${flex.row}
-
-  justify-content: space-between;
-
-  width: 100%;
-
-  font-size: 4rem;
-  font-weight: bold;
-`;
-
-const myCategory = ({ flex }: Theme) => css`
-  ${flex.row}
-
-  justify-content: space-between;
-
-  width: 56rem;
-
-  font-size: 4rem;
-`;
-
-const button = css`
-  background: transparent;
-`;
-
-export { button, list, myCategory, sideBar, title };
+export { sideBar };
