c347801651394387c19706a749c8765530562c3e
devHudi <devhudi@gmail.com>
2022-08-02 16:01:05 +0900

    test: ScheduleFixtures에서 사용되지 않은 픽스처 제거
    Co-authored-by: gudonghee2000 <gudonghee2000@gmail.com>

--- backend/src/test/java/com/allog/dallog/common/fixtures/ScheduleFixtures.java
+++ backend/src/test/java/com/allog/dallog/common/fixtures/ScheduleFixtures.java
@@ -7,10 +7,9 @@ import java.time.LocalDateTime;
 
 public class ScheduleFixtures {
 
-    /* 시작일시, 종료일시 */
+    /* 날짜 */
     public static final LocalDateTime 날짜_2022년_7월_1일_0시_0분 = LocalDateTime.of(2022, 7, 1, 0, 0);
     public static final LocalDateTime 날짜_2022년_7월_7일_16시_0분 = LocalDateTime.of(2022, 7, 7, 16, 0);
-    public static final LocalDateTime 날짜_2022년_7월_9일_16시_0분 = LocalDateTime.of(2022, 7, 9, 16, 0);
     public static final LocalDateTime 날짜_2022년_7월_31일_0시_0분 = LocalDateTime.of(2022, 7, 31, 0, 0);
     public static final LocalDateTime 날짜_2022년_7월_15일_16시_0분 = LocalDateTime.of(2022, 7, 15, 16, 0);
     public static final LocalDateTime 날짜_2022년_7월_16일_16시_0분 = LocalDateTime.of(2022, 7, 16, 16, 0);
@@ -33,12 +32,6 @@ public class ScheduleFixtures {
     public static final String 알록달록_회식_메모 = "알록달록 회식이 있어요";
     public static final ScheduleCreateRequest 알록달록_회식_생성_요청 = new ScheduleCreateRequest(알록달록_회식_제목, 알록달록_회식_시작일시,
             알록달록_회식_종료일시, 알록달록_회식_메모);
-
-
-    public static final String START_DATE_TIME_STRING = "2022-07-04T13:00";
-    public static final String END_DATE_TIME_STRING = "2022-07-05T07:00";
-    public static final int YEAR = 2022;
-    public static final int MONTH = 7;
 
     public static Schedule 알록달록_회의(final Category category) {
         return new Schedule(알록달록_회의_제목, 알록달록_회의_시작일시, 알록달록_회의_종료일시, 알록달록_회의_메모, category);
