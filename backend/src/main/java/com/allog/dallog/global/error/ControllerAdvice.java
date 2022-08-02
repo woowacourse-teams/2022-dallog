3f76ec530cc7c3bfaef36e32ae19e43ca5f6ef65
devHudi <devhudi@gmail.com>
2022-08-02 15:20:06 +0900

    feat: 잘못된 데이터 형식을 전달했을 때 발생하는 예외 핸들링 코드 추가

--- backend/src/main/java/com/allog/dallog/global/error/ControllerAdvice.java
+++ backend/src/main/java/com/allog/dallog/global/error/ControllerAdvice.java
@@ -22,6 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -75,6 +76,12 @@ public class ControllerAdvice {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleInvalidRequestBody() {
         ErrorResponse errorResponse = new ErrorResponse("잘못된 형식의 Request Body 입니다.");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch() {
+        ErrorResponse errorResponse = new ErrorResponse("잘못된 데이터 타입입니다.");
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
