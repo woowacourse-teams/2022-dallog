188685b441d762926828bdde603e412c9bb17e97
hyeonic <dev.hyeonic@gmail.com>
2022-08-02 21:00:59 +0900

    test: MemberControllerTest 추가 및 문서화 추가

--- backend/src/test/java/com/allog/dallog/domain/member/presentation/MemberControllerTest.java
+++ backend/src/test/java/com/allog/dallog/domain/member/presentation/MemberControllerTest.java
@@ -3,13 +3,18 @@ package com.allog.dallog.domain.member.presentation;
 import static com.allog.dallog.common.fixtures.AuthFixtures.더미_엑세스_토큰;
 import static com.allog.dallog.common.fixtures.AuthFixtures.토큰_정보;
 import static com.allog.dallog.common.fixtures.MemberFixtures.파랑_응답;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.allog.dallog.common.config.TestConfig;
 import com.allog.dallog.domain.auth.application.AuthService;
 import com.allog.dallog.domain.member.application.MemberService;
+import com.allog.dallog.domain.member.dto.MemberUpdateRequest;
 import com.allog.dallog.domain.member.exception.NoSuchMemberException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -27,12 +33,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureRestDocs
 @WebMvcTest(MemberController.class)
 @Import(TestConfig.class)
 class MemberControllerTest {
+
+    private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    private static final String AUTHORIZATION_HEADER_VALUE = "Bearer aaaaaaaa.bbbbbbbb.cccccccc";
 
     @Autowired
     private MockMvc mockMvc;
@@ -55,12 +65,12 @@ class MemberControllerTest {
 
         // when & then
         mockMvc.perform(get("/api/members/me")
-                        .header("Authorization", 토큰_정보)
+                        .header(AUTHORIZATION_HEADER_NAME, 토큰_정보)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
-                .andDo(document("member/me",
+                .andDo(document("members/me",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         requestHeaders(
@@ -78,15 +88,47 @@ class MemberControllerTest {
 
         // when & then
         mockMvc.perform(get("/api/members/me")
-                        .header("Authorization", 토큰_정보)
+                        .header(AUTHORIZATION_HEADER_NAME, 토큰_정보)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
-                .andDo(document("member/exception/notfound",
+                .andDo(document("members/exception/notfound",
                         preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("JWT 토큰")
+                        )
                 ))
                 .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("등록된 회원이 자신의 이름을 수정한다.")
+    @Test
+    void 등록된_회원이_자신의_이름을_수정한다() throws Exception {
+        // given
+        willDoNothing()
+                .given(memberService)
+                .update(any(), any());
+        MemberUpdateRequest 회원_수정_요청 = new MemberUpdateRequest("패트");
+
+        // when & then
+        mockMvc.perform(patch("/api/members")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(AUTHORIZATION_HEADER_NAME, AUTHORIZATION_HEADER_VALUE)
+                        .content(objectMapper.writeValueAsString(회원_수정_요청))
+                )
+                .andDo(print())
+                .andDo(document("members/update",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("JWT 토큰")
+                        ),
+                        requestFields(
+                                fieldWithPath("displayName").type(JsonFieldType.STRING).description("수정할 이름")
+                        )))
+                .andExpect(status().isNoContent());
     }
 }
