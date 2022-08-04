1935e4ede88f6e8eeb24ddf75db55bb27e03de35
jhy979 <jhy979@gmail.com>
2022-08-04 22:25:43 +0900

    feat: 구독 카테고리 수정 기능 구현

--- frontend/src/components/FilterCategoryItem/FilterCategoryItem.tsx
+++ frontend/src/components/FilterCategoryItem/FilterCategoryItem.tsx
@@ -1,10 +1,17 @@
+import { useMutation, useQueryClient } from 'react-query';
+import { useRecoilValue } from 'recoil';
+
 import useToggle from '@/hooks/useToggle';
 
 import { SubscriptionType } from '@/@types/subscription';
 
+import { userState } from '@/recoil/atoms';
+
 import Button from '@/components/@common/Button/Button';
 
-import { PALETTE } from '@/constants';
+import { CACHE_KEY, PALETTE } from '@/constants';
+
+import subscriptionApi from '@/api/subscription';
 
 import { BiPalette } from 'react-icons/bi';
 import { RiCheckboxBlankLine, RiCheckboxFill } from 'react-icons/ri';
@@ -25,17 +32,58 @@ interface FilterItemProps {
 }
 
 function FilterCategoryItem({ subscription }: FilterItemProps) {
+  const { accessToken } = useRecoilValue(userState);
   const { state: isPaletteOpen, toggleState: togglePaletteOpen } = useToggle();
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(
+    (body: Pick<SubscriptionType, 'color'> | Pick<SubscriptionType, 'checked'>) =>
+      subscriptionApi.patch(accessToken, subscription.id, body),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(CACHE_KEY.SCHEDULES);
+        queryClient.invalidateQueries(CACHE_KEY.SUBSCRIPTIONS);
+      },
+    }
+  );
+
+  const handleClickFilledCheckBox = (color: string) => {
+    mutate({
+      checked: false,
+      color,
+    });
+  };
+
+  const handleClickBlankCheckBox = (color: string) => {
+    mutate({
+      checked: true,
+      color,
+    });
+  };
+
+  const handleClickPalette = (checked: boolean, color: string) => {
+    mutate({ checked, color });
+    togglePaletteOpen();
+  };
 
   return (
     <div css={itemStyle}>
       <div css={checkBoxNameStyle}>
         {subscription.checked ? (
-          <Button>
+          <Button
+            onClick={() => {
+              handleClickFilledCheckBox(subscription.color);
+            }}
+          >
             <RiCheckboxFill size={20} color={subscription.color} />
           </Button>
         ) : (
-          <Button>
+          <Button
+            onClick={() => {
+              handleClickBlankCheckBox(subscription.color);
+            }}
+          >
             <RiCheckboxBlankLine size={20} color={subscription.color} />
           </Button>
         )}
@@ -50,7 +98,15 @@ function FilterCategoryItem({ subscription }: FilterItemProps) {
             <div css={outerStyle} onClick={togglePaletteOpen} />
             <div css={paletteStyle}>
               {PALETTE.map((color) => {
-                return <Button key={color} cssProp={colorStyle(color)}></Button>;
+                return (
+                  <Button
+                    key={color}
+                    cssProp={colorStyle(color)}
+                    onClick={() => {
+                      handleClickPalette(subscription.checked, color);
+                    }}
+                  ></Button>
+                );
               })}
             </div>
           </>
