f47ff661cc7686540f4ea78fbaea53aeabedf814
Daye Lee <508yeah@gmail.com>
2022-08-04 09:10:14 +0900

    refactor: 스타일 속성 순서 정렬

--- frontend/src/components/@common/Fieldset/Fieldset.styles.ts
+++ frontend/src/components/@common/Fieldset/Fieldset.styles.ts
@@ -3,11 +3,11 @@ import { css, Theme } from '@emotion/react';
 const fieldsetStyle = ({ flex }: Theme) => css`
   ${flex.column}
 
-  width: 100%;
-  height: 15rem;
-
   align-items: flex-start;
   gap: 2.5rem;
+
+  width: 100%;
+  height: 15rem;
 
   font-size: 5rem;
 `;
@@ -20,7 +20,6 @@ const inputStyle = ({ colors }: Theme) => css`
   padding: 3rem;
 
   width: 100%;
-
   border-radius: 8px;
   border: 1px solid ${colors.GRAY_400};
 
