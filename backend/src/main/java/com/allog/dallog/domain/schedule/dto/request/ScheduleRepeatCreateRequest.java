f99b0f300777969e61e2700777893fd5e19f4168
devHudi <devhudi@gmail.com>
2022-08-10 00:42:01 +0900

    feat: ScheduleRepeatCreateRequest에 DTO 필드 검증 어노테이션 추가

--- backend/src/main/java/com/allog/dallog/domain/schedule/dto/request/ScheduleRepeatCreateRequest.java
+++ backend/src/main/java/com/allog/dallog/domain/schedule/dto/request/ScheduleRepeatCreateRequest.java
@@ -3,11 +3,19 @@ package com.allog.dallog.domain.schedule.dto.request;
 import com.allog.dallog.domain.category.domain.Category;
 import com.allog.dallog.domain.schedule.domain.Schedule;
 import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class ScheduleRepeatCreateRequest {
 
+    @NotNull
     private final ScheduleCreateRequest schedule;
+
+    @NotBlank
     private final String repeatType;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private final LocalDate endDate;
 
     public ScheduleRepeatCreateRequest(final ScheduleCreateRequest schedule, final String repeatType,
