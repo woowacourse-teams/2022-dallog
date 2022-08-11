3ab048bf423a422c6d2b05a40eac03293396eb11
jhy979 <jhy979@gmail.com>
2022-08-11 17:37:16 +0900

    refactor: 스켈레톤 컴포넌트 props 수정

--- frontend/src/components/@common/Skeleton/Skeleton.tsx
+++ frontend/src/components/@common/Skeleton/Skeleton.tsx
@@ -4,8 +4,8 @@ import { skeletonStyle } from './Skeleton.styles';
 
 interface SkeletonProps {
   cssProp?: SerializedStyles;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
 }
 
 function Skeleton({ cssProp, width = '100%', height = '100%' }: SkeletonProps) {
