c9a2e3986c8588c6fb385f5752ea3c840fe5c257
koo <gudonghee2000@gmail.com>
2022-08-11 15:56:06 +0900

    test: RepositoryTest 객체들의 중복되는 애노테이션 제거

---
+++ backend/src/test/java/com/allog/dallog/domain/RepositoryTest.java
@@ -0,0 +1,10 @@
+package com.allog.dallog.domain;
+
+import com.allog.dallog.global.config.JpaConfig;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest
+@Import(JpaConfig.class)
+public class RepositoryTest {
+}
--- backend/src/test/java/com/allog/dallog/domain/category/domain/CategoryRepositoryTest.java
+++ backend/src/test/java/com/allog/dallog/domain/category/domain/CategoryRepositoryTest.java
@@ -13,21 +13,17 @@ import static com.allog.dallog.common.fixtures.MemberFixtures.후디;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.allog.dallog.domain.RepositoryTest;
 import com.allog.dallog.domain.member.domain.Member;
 import com.allog.dallog.domain.member.domain.MemberRepository;
-import com.allog.dallog.global.config.JpaConfig;
 import java.util.Objects;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 
-@DataJpaTest
-@Import(JpaConfig.class)
-class CategoryRepositoryTest {
+class CategoryRepositoryTest extends RepositoryTest {
 
     @Autowired
     private CategoryRepository categoryRepository;
