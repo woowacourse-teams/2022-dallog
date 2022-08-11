5a531f4a8ac0ad1809d7916ffa8c37ae726ec86d
koo <gudonghee2000@gmail.com>
2022-08-11 15:56:06 +0900

    test: RepositoryTest 객체들의 중복되는 애노테이션 제거

--- backend/src/test/java/com/allog/dallog/domain/member/domain/MemberRepositoryTest.java
+++ backend/src/test/java/com/allog/dallog/domain/member/domain/MemberRepositoryTest.java
@@ -4,18 +4,12 @@ import static com.allog.dallog.common.fixtures.MemberFixtures.파랑;
 import static com.allog.dallog.common.fixtures.MemberFixtures.파랑_이메일;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.allog.dallog.domain.member.domain.Member;
-import com.allog.dallog.domain.member.domain.MemberRepository;
-import com.allog.dallog.global.config.JpaConfig;
+import com.allog.dallog.domain.RepositoryTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 
-@DataJpaTest
-@Import(JpaConfig.class)
-class MemberRepositoryTest {
+class MemberRepositoryTest extends RepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
