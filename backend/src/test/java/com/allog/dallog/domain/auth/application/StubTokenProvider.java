19b89a4d85388f5d037b7bd239aa33a8478b1895
koo <gudonghee2000@gmail.com>
2022-08-12 15:16:35 +0900

    chore: StubTokenProvider의 불필요한 공백 제거

--- backend/src/test/java/com/allog/dallog/domain/auth/application/StubTokenProvider.java
+++ backend/src/test/java/com/allog/dallog/domain/auth/application/StubTokenProvider.java
@@ -31,7 +31,6 @@ public class StubTokenProvider implements TokenProvider {
                 .setExpiration(validity)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
-
     }
 
     @Override
