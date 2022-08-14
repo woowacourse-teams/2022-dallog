df805504d68aee740284e3de92995e9c870c46c8
jhy979 <jhy979@gmail.com>
2022-08-14 18:11:54 +0900

    fix: nth-of-child 제거

--- frontend/src/components/@common/Spinner/Spinner.styles.ts
+++ frontend/src/components/@common/Spinner/Spinner.styles.ts
@@ -19,20 +19,20 @@ const spinnerStyle = ({ colors }: Theme, size: number) => css`
     border-radius: 50%;
     border-color: ${colors.YELLOW_500} transparent transparent transparent;
 
-    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    animation: loading 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   }
 
-  & div:nth-child(1) {
+  & div:nth-of-type(1) {
     animation-delay: -0.45s;
   }
-  & div:nth-child(2) {
+  & div:nth-of-type(2) {
     animation-delay: -0.3s;
   }
-  & div:nth-child(3) {
+  & div:nth-of-type(3) {
     animation-delay: -0.15s;
   }
 
-  @keyframes lds-ring {
+  @keyframes loading {
     0% {
       transform: rotate(0deg);
     }
