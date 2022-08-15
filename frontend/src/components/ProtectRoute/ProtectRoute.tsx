4fa316705a2979cb2b6c57f571fc148c9c79671e
Daye Lee <508yeah@gmail.com>
2022-08-16 00:56:26 +0900

    feat: 로그인 사용자용 페이지 접속 시 access token의 유효성 검사

--- frontend/src/components/ProtectRoute/ProtectRoute.tsx
+++ frontend/src/components/ProtectRoute/ProtectRoute.tsx
@@ -1,37 +1,17 @@
-import { AxiosError, AxiosResponse } from 'axios';
-import { useQuery } from 'react-query';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useRecoilValue, useResetRecoilState } from 'recoil';
 
-import { userState } from '@/recoil/atoms';
+import useUserValue from '@/hooks/useUserValue';
 
-import { CACHE_KEY, PATH } from '@/constants';
-import { RESPONSE } from '@/constants/api';
-
-import { removeAccessToken } from '@/utils/storage';
-
-import loginApi from '@/api/login';
+import { PATH } from '@/constants';
 
 function ProtectRoute() {
-  const { accessToken } = useRecoilValue(userState);
-  const resetUserState = useResetRecoilState(userState);
+  const { isAuthenticating, user } = useUserValue();
 
-  const { isLoading, error } = useQuery<AxiosResponse, AxiosError>(
-    CACHE_KEY.VALIDATE,
-    () => loginApi.validate(accessToken),
-    {
-      retry: false,
-    }
-  );
-
-  if (isLoading) {
+  if (isAuthenticating) {
     return <></>;
   }
 
-  if (error?.response?.status === RESPONSE.STATUS.UNAUTHORIZED) {
-    removeAccessToken();
-    resetUserState();
-
+  if (!user.accessToken) {
     return <Navigate to={PATH.MAIN} replace />;
   }
 
