ff4ae9f0be6d5800e1dc6b1b04447ad842c01779
Daye Lee <508yeah@gmail.com>
2022-08-16 01:14:20 +0900

    refactor: 불필요한 access token 타입 제거

--- frontend/src/api/category.ts
+++ frontend/src/api/category.ts
@@ -37,7 +37,7 @@ const categoryApi = {
     return response;
   },
 
-  getMy: async (accessToken: string | null) => {
+  getMy: async (accessToken: string) => {
     const response = await dallogApi.get<CategoryType[]>(categoryApi.endpoint.my, {
       headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
       transformResponse: (res) => JSON.parse(res).categories,
@@ -46,7 +46,7 @@ const categoryApi = {
     return response;
   },
 
-  post: async (accessToken: string | null, body: Pick<CategoryType, 'name'>) => {
+  post: async (accessToken: string, body: Pick<CategoryType, 'name'>) => {
     const response = await dallogApi.post(categoryApi.endpoint.entire, body, {
       headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
     });
@@ -54,11 +54,7 @@ const categoryApi = {
     return response;
   },
 
-  patch: async (
-    accessToken: string | null,
-    categoryId: number,
-    body: Pick<CategoryType, 'name'>
-  ) => {
+  patch: async (accessToken: string, categoryId: number, body: Pick<CategoryType, 'name'>) => {
     const response = await dallogApi.patch(`${categoryApi.endpoint.entire}/${categoryId}`, body, {
       headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
     });
@@ -66,7 +62,7 @@ const categoryApi = {
     return response;
   },
 
-  delete: async (accessToken: string | null, categoryId: number) => {
+  delete: async (accessToken: string, categoryId: number) => {
     const response = await dallogApi.delete(`${categoryApi.endpoint.entire}/${categoryId}`, {
       headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
     });
