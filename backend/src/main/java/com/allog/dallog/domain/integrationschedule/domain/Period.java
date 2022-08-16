638d25ef137e172b75aab23a51284d0e8d29eb1b
devHudi <devhudi@gmail.com>
2022-08-16 14:17:38 +0900

    feat: Period를 다른 Period로 자르는 기능 구현

--- backend/src/main/java/com/allog/dallog/domain/integrationschedule/domain/Period.java
+++ backend/src/main/java/com/allog/dallog/domain/integrationschedule/domain/Period.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Period {
 
@@ -35,11 +38,59 @@ public class Period {
         return ChronoUnit.MINUTES.between(startTime, endTime) % ONE_HOUR;
     }
 
+    public Period plusMinutes(final int minutes) {
+        LocalDateTime startDateTime = this.startDateTime.plusMinutes(minutes);
+        LocalDateTime endDateTime = this.endDateTime.plusMinutes(minutes);
+        return new Period(startDateTime, endDateTime);
+    }
+
+    public List<Period> slice(final Period otherPeriod) {
+        if (isNotOverlapped(otherPeriod)) {
+            return List.of(this);
+        }
+
+        return sliceByOtherPeriod(otherPeriod);
+    }
+
+    private List<Period> sliceByOtherPeriod(final Period otherPeriod) {
+        List<Period> periods = new ArrayList<>();
+        if (startDateTime.isBefore(otherPeriod.startDateTime)) {
+            periods.add(new Period(startDateTime, otherPeriod.startDateTime));
+        }
+
+        if (otherPeriod.endDateTime.isBefore(endDateTime)) {
+            periods.add(new Period(otherPeriod.endDateTime, endDateTime));
+        }
+
+        return periods;
+    }
+
+    private boolean isNotOverlapped(final Period otherPeriod) {
+        return otherPeriod.endDateTime.isBefore(startDateTime) || endDateTime.isBefore(otherPeriod.startDateTime);
+    }
+
     public LocalDateTime getStartDateTime() {
         return startDateTime;
     }
 
     public LocalDateTime getEndDateTime() {
         return endDateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Period period = (Period) o;
+        return Objects.equals(startDateTime, period.startDateTime) && Objects.equals(endDateTime, period.endDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startDateTime, endDateTime);
     }
 }
--- backend/src/test/java/com/allog/dallog/domain/integrationschedule/domain/PeriodTest.java
+++ backend/src/test/java/com/allog/dallog/domain/integrationschedule/domain/PeriodTest.java
@@ -1,8 +1,10 @@
 package com.allog.dallog.domain.integrationschedule.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -54,5 +56,119 @@ class PeriodTest {
 
         // then
         assertThat(minuteDifference).isEqualTo(2);
+    }
+
+    @DisplayName("기간 뺄셈시 상대 기간이 우측에 걸쳐있을 때의 결과를 계산한다.")
+    @Test
+    void 기간_뺄셈시_상대_기간이_우측에_걸쳐있을_때의_결과를_계산한다() {
+        // given
+        LocalDateTime baseStartDateTime = LocalDateTime.of(2022, 8, 1, 0, 0);
+        LocalDateTime baseEndDateTime = LocalDateTime.of(2022, 8, 2, 23, 59);
+        Period basePeriod = new Period(baseStartDateTime, baseEndDateTime);
+
+        LocalDateTime otherStartDateTime = LocalDateTime.of(2022, 8, 1, 18, 0);
+        LocalDateTime otherEndDateTime = LocalDateTime.of(2022, 8, 3, 18, 0);
+        Period otherPeriod = new Period(otherStartDateTime, otherEndDateTime);
+
+        // when
+        List<Period> actual = basePeriod.slice(otherPeriod);
+
+        // then
+        assertAll(() -> {
+            assertThat(actual).hasSize(1);
+            assertThat(actual.get(0).getStartDateTime()).isEqualTo(baseStartDateTime);
+            assertThat(actual.get(0).getEndDateTime()).isEqualTo(otherStartDateTime);
+        });
+    }
+
+    @DisplayName("기간 뺄셈시 상대 기간이 좌측에 걸쳐있을 때의 결과를 계산한다.")
+    @Test
+    void 기간_뺄셈시_상대_기간이_좌측에_걸쳐있을_때의_결과를_계산한다() {
+        // given
+        LocalDateTime baseStartDateTime = LocalDateTime.of(2022, 8, 2, 0, 0);
+        LocalDateTime baseEndDateTime = LocalDateTime.of(2022, 8, 3, 23, 59);
+        Period basePeriod = new Period(baseStartDateTime, baseEndDateTime);
+
+        LocalDateTime otherStartDateTime = LocalDateTime.of(2022, 8, 1, 18, 0);
+        LocalDateTime otherEndDateTime = LocalDateTime.of(2022, 8, 2, 18, 0);
+        Period otherPeriod = new Period(otherStartDateTime, otherEndDateTime);
+
+        // when
+        List<Period> actual = basePeriod.slice(otherPeriod);
+
+        // then
+        assertAll(() -> {
+            assertThat(actual).hasSize(1);
+            assertThat(actual.get(0).getStartDateTime()).isEqualTo(otherEndDateTime);
+            assertThat(actual.get(0).getEndDateTime()).isEqualTo(baseEndDateTime);
+        });
+    }
+
+    @DisplayName("기간 뺄셈시 상대 기간이 안쪽에 포함될때 결과를 계산한다.")
+    @Test
+    void 기간_뺄셈시_상대_기간이_안쪽에_포함될때_결과를_계산한다() {
+        // given
+        LocalDateTime baseStartDateTime = LocalDateTime.of(2022, 8, 1, 0, 0);
+        LocalDateTime baseEndDateTime = LocalDateTime.of(2022, 8, 3, 23, 59);
+        Period basePeriod = new Period(baseStartDateTime, baseEndDateTime);
+
+        LocalDateTime otherStartDateTime = LocalDateTime.of(2022, 8, 1, 18, 0);
+        LocalDateTime otherEndDateTime = LocalDateTime.of(2022, 8, 2, 18, 0);
+        Period otherPeriod = new Period(otherStartDateTime, otherEndDateTime);
+
+        // when
+        List<Period> actual = basePeriod.slice(otherPeriod);
+
+        // then
+        assertAll(() -> {
+            assertThat(actual).hasSize(2);
+            assertThat(actual.get(0).getStartDateTime()).isEqualTo(baseStartDateTime);
+            assertThat(actual.get(0).getEndDateTime()).isEqualTo(otherStartDateTime);
+            assertThat(actual.get(1).getStartDateTime()).isEqualTo(otherEndDateTime);
+            assertThat(actual.get(1).getEndDateTime()).isEqualTo(baseEndDateTime);
+        });
+    }
+
+    @DisplayName("기간 뺄셈시 상대 기간과 완벽히 일치하면 빈 리스트를 반환한다.")
+    @Test
+    void 기간_뺄셈시_상대_기간과_완벽히_일치하면_빈_리스트를_반환한다() {
+        // given
+        LocalDateTime baseStartDateTime = LocalDateTime.of(2022, 8, 1, 0, 0);
+        LocalDateTime baseEndDateTime = LocalDateTime.of(2022, 8, 3, 23, 59);
+        Period basePeriod = new Period(baseStartDateTime, baseEndDateTime);
+
+        LocalDateTime otherStartDateTime = LocalDateTime.of(2022, 8, 1, 0, 0);
+        LocalDateTime otherEndDateTime = LocalDateTime.of(2022, 8, 3, 23, 59);
+        Period otherPeriod = new Period(otherStartDateTime, otherEndDateTime);
+
+        // when
+        List<Period> actual = basePeriod.slice(otherPeriod);
+
+        // then
+        assertAll(() -> {
+            assertThat(actual).hasSize(0);
+        });
+    }
+
+    @DisplayName("기간 뺄셈시 상대 기간과 겹치지 않으면 자기자신을 리스트로 반환한다.")
+    @Test
+    void 기간_뺄셈시_상대_기간과_겹치지_않으면_자기자신을_리스트로_반환한다() {
+        // given
+        LocalDateTime baseStartDateTime = LocalDateTime.of(2022, 8, 1, 0, 0);
+        LocalDateTime baseEndDateTime = LocalDateTime.of(2022, 8, 2, 0, 0);
+        Period basePeriod = new Period(baseStartDateTime, baseEndDateTime);
+
+        LocalDateTime otherStartDateTime = LocalDateTime.of(2022, 8, 3, 0, 0);
+        LocalDateTime otherEndDateTime = LocalDateTime.of(2022, 8, 3, 23, 59);
+        Period otherPeriod = new Period(otherStartDateTime, otherEndDateTime);
+
+        // when
+        List<Period> actual = basePeriod.slice(otherPeriod);
+
+        // then
+        assertAll(() -> {
+            assertThat(actual).hasSize(1);
+            assertThat(actual.get(0)).isEqualTo(basePeriod);
+        });
     }
 }
