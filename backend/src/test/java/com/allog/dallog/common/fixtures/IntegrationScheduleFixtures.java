e7bfcd7d8b1f626b617d72c7f8a3a36b1158cb75
hyeonic <dev.hyeonic@gmail.com>
2022-08-16 16:11:55 +0900

    feat: 외부 캘린더 조회 반환 타입 변경

---
+++ backend/src/test/java/com/allog/dallog/common/fixtures/IntegrationScheduleFixtures.java
@@ -0,0 +1,14 @@
+package com.allog.dallog.common.fixtures;
+
+import com.allog.dallog.domain.integrationschedule.domain.IntegrationSchedule;
+import com.allog.dallog.domain.integrationschedule.domain.Period;
+import java.time.LocalDateTime;
+
+public class IntegrationScheduleFixtures {
+
+    public static final IntegrationSchedule 레벨3_방학 = new IntegrationSchedule("gsgadfgqwrtqwerfgasdasdasd", 1L,
+            "레벨3 방학", new Period(LocalDateTime.of(2022, 8, 20, 00, 00), LocalDateTime.of(2022, 8, 20, 00, 00)), "");
+
+    public static final IntegrationSchedule 포수타 = new IntegrationSchedule("asgasgasfgadfgdf", 1L,
+            "포수타", new Period(LocalDateTime.of(2022, 8, 12, 14, 00), LocalDateTime.of(2022, 8, 12, 14, 30)), "");
+}
