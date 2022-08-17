99be083f80ec656eb097a6c013f2e1c2520edde9
jhy979 <jhy979@gmail.com>
2022-08-17 12:11:50 +0900

    feat: NotFound 페이지 진입 시 사이드바 감추기

--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,9 @@
 import { useTheme } from '@emotion/react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+
+import { sideBarSelector } from '@/recoil/selectors';
 
 import Button from '@/components/@common/Button/Button';
 
@@ -9,12 +13,18 @@ import { buttonStyle, layoutStyle, textStyle } from './NotFoundPage.styles';
 
 function NotFoundPage() {
   const theme = useTheme();
+
   const navigation = useNavigate();
+
+  const [isSideBarOpen, toggleSideBarOpen] = useRecoilState(sideBarSelector);
 
   const handleClickReturnButton = () => {
     navigation(PATH.MAIN);
   };
 
+  useEffect(() => {
+    toggleSideBarOpen(isSideBarOpen);
+  }, []);
   return (
     <div css={layoutStyle}>
       <span css={textStyle(theme, '40rem')}>(⊙_⊙;)</span>
