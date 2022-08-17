b984e8b0b9f1bd96904dbfe8504b0b5e510d4613
summerlunaa <summerlunaa9@gmail.com>
2022-08-17 14:20:00 +0900

    test: CategoryType 테스트 추가

--- backend/src/test/java/com/allog/dallog/domain/category/domain/CategoryTypeTest.java
+++ backend/src/test/java/com/allog/dallog/domain/category/domain/CategoryTypeTest.java
@@ -1,9 +1,12 @@
 package com.allog.dallog.domain.category.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.allog.dallog.domain.category.exception.NoSuchCategoryException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -18,5 +21,16 @@ public class CategoryTypeTest {
             assertThat(CategoryType.from(categoryType.name())).isEqualTo(categoryType);
             assertThat(CategoryType.from(categoryType.name().toLowerCase())).isEqualTo(categoryType);
         });
+    }
+
+    @DisplayName("존재하지 않는 카테고리 종류인 경우 예외를 던진다.")
+    @Test
+    void 존재하지_않는_카테고리_종류인_경우_예외를_던진다() {
+        // given
+        String notExistingCategoryType = "존재하지 않는 카테고리 종류";
+
+        // when & then
+        assertThatThrownBy(() -> CategoryType.from(notExistingCategoryType))
+                .isInstanceOf(NoSuchCategoryException.class);
     }
 }
