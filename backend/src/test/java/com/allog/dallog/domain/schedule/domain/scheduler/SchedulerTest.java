bd0fde8efe2eb8c4f20b2aa5b4c0459fb2b36e77
devHudi <devhudi@gmail.com>
2022-08-17 12:53:16 +0900

    test: CategoryType 대응

--- backend/src/test/java/com/allog/dallog/domain/schedule/domain/scheduler/SchedulerTest.java
+++ backend/src/test/java/com/allog/dallog/domain/schedule/domain/scheduler/SchedulerTest.java
@@ -42,21 +42,21 @@ class SchedulerTest {
         String 일정_메모 = "일정 메모";
 
         IntegrationSchedule 일정1 = new IntegrationSchedule("1", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_7일_16시_0분,
-                날짜_2022년_7월_10일_0시_0분, 일정_메모);
+                날짜_2022년_7월_10일_0시_0분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정2 = new IntegrationSchedule("2", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_10일_11시_59분,
-                날짜_2022년_7월_15일_16시_0분, 일정_메모);
+                날짜_2022년_7월_15일_16시_0분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정3 = new IntegrationSchedule("3", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_16일_16시_0분,
-                날짜_2022년_7월_16일_16시_1분, 일정_메모);
+                날짜_2022년_7월_16일_16시_1분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정4 = new IntegrationSchedule("4", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_16일_18시_0분,
-                날짜_2022년_7월_16일_20시_0분, 일정_메모);
+                날짜_2022년_7월_16일_20시_0분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정5 = new IntegrationSchedule("5", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_16일_20시_0분,
-                날짜_2022년_7월_20일_0시_0분, 일정_메모);
+                날짜_2022년_7월_20일_0시_0분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정6 = new IntegrationSchedule("6", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_20일_11시_59분,
-                날짜_2022년_7월_27일_0시_0분, 일정_메모);
+                날짜_2022년_7월_27일_0시_0분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정7 = new IntegrationSchedule("7", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_27일_11시_59분,
-                날짜_2022년_7월_31일_0시_0분, 일정_메모);
+                날짜_2022년_7월_31일_0시_0분, 일정_메모, "NORMAL");
         IntegrationSchedule 일정8 = new IntegrationSchedule("8", 공통_일정.getId(), 일정_제목, 날짜_2022년_7월_31일_0시_0분,
-                날짜_2022년_8월_15일_14시_0분, 일정_메모);
+                날짜_2022년_8월_15일_14시_0분, 일정_메모, "NORMAL");
 
         List<IntegrationSchedule> 일정_목록 = List.of(일정1, 일정2, 일정3, 일정4, 일정5, 일정6, 일정7, 일정8);
 
