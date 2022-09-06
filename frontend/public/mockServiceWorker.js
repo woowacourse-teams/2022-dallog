5af8c67049a7ef14007da43bafca88eb0aba58d1
Daye Lee <508yeah@gmail.com>
2022-09-07 08:44:23 +0900

    chore: 패키지 재설치

--- frontend/public/mockServiceWorker.js
+++ frontend/public/mockServiceWorker.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (0.43.0).
+ * Mock Service Worker (0.43.1).
  * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
