14f538375dc418e335bc4627a90c20c4106ebe3a
jhy979 <jhy979@gmail.com>
2022-09-10 22:26:11 +0900

    refactor: validation 함수 리팩토링

--- frontend/src/validation/index.ts
+++ frontend/src/validation/index.ts
@@ -1,24 +1,17 @@
 import { VALIDATION_STRING } from '@/constants/validate';
 
-const validateLength = (target: string, min: number, max: number) => {
-  return min <= target.length && target.length <= max;
-};
+const validateLength = (target: string, min: number, max: number) =>
+  min <= target.length && target.length <= max;
 
-const validateNotEmpty = (target: string) => {
-  return target.length > 0;
-};
+const validateNotEmpty = (target: string) => target.length > 0;
 
-const validateNotEqualString = (target: string, comparisonTarget: string) => {
-  return target.trim() !== comparisonTarget;
-};
+const validateNotEqualString = (target: string, comparisonTarget: string) =>
+  target.trim() !== comparisonTarget;
 
-const validateStartEndDateTime = (startDate: string, endDate: string) => {
-  return startDate <= endDate;
-};
+const validateStartEndDateTime = (startDate: string, endDate: string) => startDate <= endDate;
 
-const validateWithdrawalCondition = (value: string) => {
-  return value === VALIDATION_STRING.WITHDRAWAL;
-};
+const validateWithdrawalCondition = (value: string) => value === VALIDATION_STRING.WITHDRAWAL;
+
 export {
   validateLength,
   validateNotEmpty,
