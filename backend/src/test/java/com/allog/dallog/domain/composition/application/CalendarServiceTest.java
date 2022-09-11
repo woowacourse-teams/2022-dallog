dfdcdce0a39325a6023789e5fae6515a42b0c763
hyeonic <dev.hyeonic@gmail.com>
2022-09-11 20:27:01 +0900

    test: 로직 변경에 맞춰 CalendarServiceTest 수정

--- backend/src/test/java/com/allog/dallog/domain/composition/application/CalendarServiceTest.java
+++ backend/src/test/java/com/allog/dallog/domain/composition/application/CalendarServiceTest.java
@@ -1,5 +1,7 @@
 package com.allog.dallog.domain.composition.application;
 
+import static com.allog.dallog.common.fixtures.AuthFixtures.MEMBER_이메일;
+import static com.allog.dallog.common.fixtures.AuthFixtures.MEMBER_인증_코드_토큰_요청;
 import static com.allog.dallog.common.fixtures.CategoryFixtures.BE_일정_생성_요청;
 import static com.allog.dallog.common.fixtures.CategoryFixtures.FE_일정_생성_요청;
 import static com.allog.dallog.common.fixtures.CategoryFixtures.공통_일정_생성_요청;
@@ -29,8 +31,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.allog.dallog.common.annotation.ServiceTest;
-import com.allog.dallog.domain.auth.domain.OAuthToken;
-import com.allog.dallog.domain.auth.domain.OAuthTokenRepository;
+import com.allog.dallog.domain.auth.application.AuthService;
 import com.allog.dallog.domain.category.application.CategoryService;
 import com.allog.dallog.domain.category.domain.Category;
 import com.allog.dallog.domain.category.domain.ExternalCategoryDetail;
@@ -38,6 +39,7 @@ import com.allog.dallog.domain.category.domain.ExternalCategoryDetailRepository;
 import com.allog.dallog.domain.category.dto.response.CategoryResponse;
 import com.allog.dallog.domain.integrationschedule.domain.IntegrationSchedule;
 import com.allog.dallog.domain.member.application.MemberService;
+import com.allog.dallog.domain.member.domain.Member;
 import com.allog.dallog.domain.member.dto.MemberResponse;
 import com.allog.dallog.domain.schedule.application.ScheduleService;
 import com.allog.dallog.domain.schedule.dto.request.DateRangeRequest;
@@ -56,13 +58,13 @@ class CalendarServiceTest extends ServiceTest {
     private CalendarService calendarService;
 
     @Autowired
+    private AuthService authService;
+
+    @Autowired
     private MemberService memberService;
 
     @Autowired
     private CategoryService categoryService;
-
-    @Autowired
-    private OAuthTokenRepository oAuthTokenRepository;
 
     @Autowired
     private ExternalCategoryDetailRepository externalCategoryDetailRepository;
@@ -77,52 +79,52 @@ class CalendarServiceTest extends ServiceTest {
     @Test
     void 시작일시와_종료일시로_유저의_캘린더를_일정_유형에_따라_분류하고_정렬하여_반환한다() {
         // given
-        MemberResponse 후디 = memberService.save(후디());
-        oAuthTokenRepository.save(new OAuthToken(memberService.getMember(후디.getId()), "wegwefaasdasdasda"));
+        authService.generateToken(MEMBER_인증_코드_토큰_요청());
+        Member member = memberService.getByEmail(MEMBER_이메일);
 
-        CategoryResponse BE_일정_응답 = categoryService.save(후디.getId(), BE_일정_생성_요청);
+        CategoryResponse BE_일정_응답 = categoryService.save(member.getId(), BE_일정_생성_요청);
         Category BE_일정 = categoryService.getCategory(BE_일정_응답.getId());
 
-        subscriptionService.save(후디.getId(), BE_일정.getId());
+        subscriptionService.save(member.getId(), BE_일정.getId());
 
         /* 장기간 일정 */
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("장기간 첫번째", 날짜_2022년_7월_1일_0시_0분, 날짜_2022년_8월_15일_14시_0분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("장기간 두번째", 날짜_2022년_7월_1일_0시_0분, 날짜_2022년_7월_31일_0시_0분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("장기간 세번째", 날짜_2022년_7월_1일_0시_0분, 날짜_2022년_7월_16일_16시_1분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("장기간 네번째", 날짜_2022년_7월_7일_16시_0분, 날짜_2022년_7월_15일_16시_0분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("장기간 다섯번째", 날짜_2022년_7월_31일_0시_0분, 날짜_2022년_8월_15일_17시_0분, ""));
 
         /* 종일 일정 */
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("종일 첫번째", 날짜_2022년_7월_10일_0시_0분, 날짜_2022년_7월_10일_11시_59분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("종일 두번째", 날짜_2022년_7월_20일_0시_0분, 날짜_2022년_7월_20일_11시_59분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("종일 세번째", 날짜_2022년_7월_27일_0시_0분, 날짜_2022년_7월_27일_11시_59분, ""));
 
         /* 몇시간 일정 */
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("몇시간 첫번째", 날짜_2022년_7월_16일_16시_0분, 날짜_2022년_7월_16일_20시_0분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("몇시간 두번째", 날짜_2022년_7월_16일_16시_0분, 날짜_2022년_7월_16일_18시_0분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("몇시간 세번째", 날짜_2022년_7월_16일_16시_0분, 날짜_2022년_7월_16일_16시_1분, ""));
-        scheduleService.save(후디.getId(), BE_일정.getId(),
+        scheduleService.save(member.getId(), BE_일정.getId(),
                 new ScheduleCreateRequest("몇시간 네번째", 날짜_2022년_7월_16일_18시_0분, 날짜_2022년_7월_16일_18시_0분, ""));
 
-        CategoryResponse 우아한테크코스_외부_일정_응답 = categoryService.save(후디.getId(), 우아한테크코스_외부_일정_생성_요청);
+        CategoryResponse 우아한테크코스_외부_일정_응답 = categoryService.save(member.getId(), 우아한테크코스_외부_일정_생성_요청);
         Category 우아한테크코스 = categoryService.getCategory(우아한테크코스_외부_일정_응답.getId());
         externalCategoryDetailRepository.save(new ExternalCategoryDetail(우아한테크코스, "dfggsdfasdasadsgs"));
 
-        subscriptionService.save(후디.getId(), 우아한테크코스.getId());
+        subscriptionService.save(member.getId(), 우아한테크코스.getId());
 
         // when
-        MemberScheduleResponses memberScheduleResponses = calendarService.findSchedulesByMemberId(후디.getId(),
+        MemberScheduleResponses memberScheduleResponses = calendarService.findSchedulesByMemberId(member.getId(),
                 new DateRangeRequest("2022-07-01T00:00", "2022-08-15T23:59"));
 
         // then
