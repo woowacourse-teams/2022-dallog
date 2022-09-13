9170d975b17d564b894f40606aceb90eced71a1d
summerlunaa <summerlunaa9@gmail.com>
2022-09-13 20:21:30 +0900

    refactor: dto에서 사용하지 않는 필드 삭제

--- backend/src/main/java/com/allog/dallog/infrastructure/oauth/dto/GoogleCalendarListResponse.java
+++ backend/src/main/java/com/allog/dallog/infrastructure/oauth/dto/GoogleCalendarListResponse.java
@@ -4,38 +4,13 @@ import java.util.List;
 
 public class GoogleCalendarListResponse {
 
-    private String kind;
-    private String etag;
-    private String nextPageToken;
-    private String nextSyncToken;
     private List<GoogleCalendarResponse> items;
 
     private GoogleCalendarListResponse() {
     }
 
-    public GoogleCalendarListResponse(final String kind, final String etag, final String nextPageToken,
-                                      final String nextSyncToken, final List<GoogleCalendarResponse> items) {
-        this.kind = kind;
-        this.etag = etag;
-        this.nextPageToken = nextPageToken;
-        this.nextSyncToken = nextSyncToken;
+    public GoogleCalendarListResponse(final List<GoogleCalendarResponse> items) {
         this.items = items;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public String getEtag() {
-        return etag;
-    }
-
-    public String getNextPageToken() {
-        return nextPageToken;
-    }
-
-    public String getNextSyncToken() {
-        return nextSyncToken;
     }
 
     public List<GoogleCalendarResponse> getItems() {
--- backend/src/main/java/com/allog/dallog/infrastructure/oauth/dto/GoogleTokenResponse.java
+++ backend/src/main/java/com/allog/dallog/infrastructure/oauth/dto/GoogleTokenResponse.java
@@ -6,28 +6,15 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class GoogleTokenResponse {
 
-    private String accessToken;
     private String refreshToken;
     private String idToken;
-    private String expiresIn;
-    private String tokenType;
-    private String scope;
 
     private GoogleTokenResponse() {
     }
 
-    public GoogleTokenResponse(final String accessToken, final String refreshToken, final String idToken,
-                               final String expiresIn, final String scope, final String tokenType) {
-        this.accessToken = accessToken;
+    public GoogleTokenResponse(final String refreshToken, final String idToken) {
         this.refreshToken = refreshToken;
         this.idToken = idToken;
-        this.expiresIn = expiresIn;
-        this.scope = scope;
-        this.tokenType = tokenType;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
     }
 
     public String getRefreshToken() {
@@ -36,17 +23,5 @@ public class GoogleTokenResponse {
 
     public String getIdToken() {
         return idToken;
-    }
-
-    public String getExpiresIn() {
-        return expiresIn;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public String getTokenType() {
-        return tokenType;
     }
 }
