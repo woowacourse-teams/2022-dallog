f2c0b7b566f1246bee2087b0389a94cb2b3bc9d4
hyeonic <dev.hyeonic@gmail.com>
2022-09-13 16:30:41 +0900

    feat: MemberRepository 예외 처리를 위한 default 메서드 추가

--- backend/src/main/java/com/allog/dallog/domain/member/domain/MemberRepository.java
+++ backend/src/main/java/com/allog/dallog/domain/member/domain/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.allog.dallog.domain.member.domain;
 
+import com.allog.dallog.domain.member.exception.NoSuchMemberException;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(final String email);
 
     boolean existsByEmail(final String email);
+
+    default Member getById(final Long id) {
+        return findById(id)
+                .orElseThrow(NoSuchMemberException::new);
+    }
+
+    default Member getByEmail(final String email) {
+        return findByEmail(email)
+                .orElseThrow(NoSuchMemberException::new);
+    }
 }
