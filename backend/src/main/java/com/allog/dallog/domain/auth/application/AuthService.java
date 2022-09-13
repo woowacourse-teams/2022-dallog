903e8ce0807757e8c3cb56be5414390e6db56579
hyeonic <dev.hyeonic@gmail.com>
2022-09-13 16:31:49 +0900

    feat: 토큰 생성 로직 트랜잭션 처리 개선

--- backend/src/main/java/com/allog/dallog/domain/auth/application/AuthService.java
+++ backend/src/main/java/com/allog/dallog/domain/auth/application/AuthService.java
@@ -1,14 +1,23 @@
 package com.allog.dallog.domain.auth.application;
 
+import static com.allog.dallog.domain.category.domain.CategoryType.PERSONAL;
+
 import com.allog.dallog.domain.auth.domain.OAuthToken;
 import com.allog.dallog.domain.auth.domain.OAuthTokenRepository;
 import com.allog.dallog.domain.auth.dto.OAuthMember;
 import com.allog.dallog.domain.auth.dto.request.TokenRequest;
 import com.allog.dallog.domain.auth.dto.response.TokenResponse;
-import com.allog.dallog.domain.auth.exception.NoSuchOAuthTokenException;
-import com.allog.dallog.domain.composition.application.RegisterService;
-import com.allog.dallog.domain.member.application.MemberService;
+import com.allog.dallog.domain.category.domain.Category;
+import com.allog.dallog.domain.category.domain.CategoryRepository;
 import com.allog.dallog.domain.member.domain.Member;
+import com.allog.dallog.domain.member.domain.MemberRepository;
+import com.allog.dallog.domain.member.domain.SocialType;
+import com.allog.dallog.domain.member.exception.NoSuchMemberException;
+import com.allog.dallog.domain.subscription.domain.Color;
+import com.allog.dallog.domain.subscription.domain.ColorPickerStrategy;
+import com.allog.dallog.domain.subscription.domain.Subscription;
+import com.allog.dallog.domain.subscription.domain.SubscriptionRepository;
+import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,21 +25,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AuthService {
 
+    private static final String PERSONAL_CATEGORY_NAME = "내 일정";
+    private static final ColorPickerStrategy PICK_RANDOM_STRATEGY = () -> ThreadLocalRandom.current()
+            .nextInt(Color.values().length);
+
+    private final MemberRepository memberRepository;
+    private final CategoryRepository categoryRepository;
+    private final SubscriptionRepository subscriptionRepository;
+    private final OAuthTokenRepository oAuthTokenRepository;
     private final OAuthUri oAuthUri;
     private final OAuthClient oAuthClient;
-    private final MemberService memberService;
-    private final RegisterService registerService;
-    private final OAuthTokenRepository oAuthTokenRepository;
     private final TokenProvider tokenProvider;
 
-    public AuthService(final OAuthUri oAuthUri, final OAuthClient oAuthClient, final MemberService memberService,
-                       final RegisterService registerService, final OAuthTokenRepository oAuthTokenRepository,
-                       final TokenProvider tokenProvider) {
+    public AuthService(final MemberRepository memberRepository, final CategoryRepository categoryRepository,
+                       final SubscriptionRepository subscriptionRepository,
+                       final OAuthTokenRepository oAuthTokenRepository, final OAuthUri oAuthUri,
+                       final OAuthClient oAuthClient, final TokenProvider tokenProvider) {
+        this.memberRepository = memberRepository;
+        this.categoryRepository = categoryRepository;
+        this.subscriptionRepository = subscriptionRepository;
+        this.oAuthTokenRepository = oAuthTokenRepository;
         this.oAuthUri = oAuthUri;
         this.oAuthClient = oAuthClient;
-        this.memberService = memberService;
-        this.registerService = registerService;
-        this.oAuthTokenRepository = oAuthTokenRepository;
         this.tokenProvider = tokenProvider;
     }
 
@@ -44,7 +60,13 @@ public class AuthService {
         String redirectUri = tokenRequest.getRedirectUri();
 
         OAuthMember oAuthMember = oAuthClient.getOAuthMember(code, redirectUri);
-        Member foundMember = getMember(oAuthMember);
+
+        if (!memberRepository.existsByEmail(oAuthMember.getEmail())) {
+            Member savedMember = memberRepository.save(parseMember(oAuthMember));
+            Category savedCategory = saveCategory(savedMember);
+            saveSubscription(savedMember, savedCategory);
+        }
+        Member foundMember = memberRepository.getByEmail(oAuthMember.getEmail());
 
         OAuthToken oAuthToken = getOAuthToken(oAuthMember, foundMember);
         oAuthToken.change(oAuthMember.getRefreshToken());
@@ -53,12 +75,18 @@ public class AuthService {
         return new TokenResponse(accessToken);
     }
 
-    private Member getMember(final OAuthMember oAuthMember) {
-        if (!memberService.existsByEmail(oAuthMember.getEmail())) {
-            registerService.register(oAuthMember);
-        }
+    private Category saveCategory(final Member savedMember) {
+        return categoryRepository.save(new Category(PERSONAL_CATEGORY_NAME, savedMember, PERSONAL));
+    }
 
-        return memberService.getByEmail(oAuthMember.getEmail());
+    private Subscription saveSubscription(final Member savedMember, final Category savedCategory) {
+        Color randomColor = Color.pickAny(PICK_RANDOM_STRATEGY);
+        return subscriptionRepository.save(new Subscription(savedMember, savedCategory, randomColor));
+    }
+
+    private Member parseMember(final OAuthMember oAuthMember) {
+        return new Member(oAuthMember.getEmail(), oAuthMember.getDisplayName(), oAuthMember.getProfileImageUrl(),
+                SocialType.GOOGLE);
     }
 
     private OAuthToken getOAuthToken(final OAuthMember oAuthMember, final Member foundMember) {
@@ -66,14 +94,16 @@ public class AuthService {
             oAuthTokenRepository.save(new OAuthToken(foundMember, oAuthMember.getRefreshToken()));
         }
 
-        return oAuthTokenRepository.findByMemberId(foundMember.getId())
-                .orElseThrow(NoSuchOAuthTokenException::new);
+        return oAuthTokenRepository.getByMemberId(foundMember.getId());
     }
 
     public Long extractMemberId(final String accessToken) {
         tokenProvider.validateToken(accessToken);
         Long memberId = Long.valueOf(tokenProvider.getPayload(accessToken));
-        memberService.validateExistsMember(memberId);
+        if (!memberRepository.existsById(memberId)) {
+            throw new NoSuchMemberException();
+        }
+
         return memberId;
     }
 }
