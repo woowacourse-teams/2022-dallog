1134d6b12198e91de5f77b16e509d3ef80f85365
jhy979 <jhy979@gmail.com>
2022-09-19 08:32:11 +0900

    refactor: date 상수 리팩토링

--- frontend/src/constants/date.ts
+++ frontend/src/constants/date.ts
@@ -1,3 +1,5 @@
+import { zeroFill } from '@/utils';
+
 const DATE_TIME = {
   START: '00:00',
   END: '23:59',
@@ -8,8 +10,6 @@ const DAYS = ['일', '월', '화', '수', '목', '금', '토'];
 const TIMES = new Array(48)
   .fill(0)
   .map((_, arrIdx) => Math.floor(arrIdx / 2).toString())
-  .map((hour, hourIdx) =>
-    hourIdx % 2 === 0 ? `${hour.padStart(2, '0')}:00` : `${hour.padStart(2, '0')}:30`
-  );
+  .map((hour, hourIdx) => (hourIdx % 2 === 0 ? `${zeroFill(hour)}:00` : `${zeroFill(hour)}:30`));
 
 export { DATE_TIME, DAYS, TIMES };
