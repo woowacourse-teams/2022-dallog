9874ba98acac1b48b6db358d18cefaf11278e60d
hyeonic <dev.hyeonic@gmail.com>
2022-09-22 15:28:10 +0900

    refactor: 일정 조율 기능 제거

--- backend/src/main/java/com/allog/dallog/domain/schedule/application/ScheduleService.java
+++ backend/src/main/java/com/allog/dallog/domain/schedule/application/ScheduleService.java
@@ -1,29 +1,21 @@
 package com.allog.dallog.domain.schedule.application;
 
-import com.allog.dallog.domain.auth.domain.OAuthToken;
 import com.allog.dallog.domain.auth.domain.OAuthTokenRepository;
 import com.allog.dallog.domain.category.domain.Category;
 import com.allog.dallog.domain.category.domain.CategoryRepository;
-import com.allog.dallog.domain.category.domain.ExternalCategoryDetail;
 import com.allog.dallog.domain.category.domain.ExternalCategoryDetailRepository;
 import com.allog.dallog.domain.member.domain.Member;
 import com.allog.dallog.domain.member.domain.MemberRepository;
 import com.allog.dallog.domain.schedule.domain.IntegrationSchedule;
 import com.allog.dallog.domain.schedule.domain.Schedule;
 import com.allog.dallog.domain.schedule.domain.ScheduleRepository;
-import com.allog.dallog.domain.schedule.dto.AvailablePeriodMaterial;
-import com.allog.dallog.domain.schedule.dto.ExternalRequestMaterial;
 import com.allog.dallog.domain.schedule.dto.request.DateRangeRequest;
 import com.allog.dallog.domain.schedule.dto.request.ScheduleCreateRequest;
 import com.allog.dallog.domain.schedule.dto.request.ScheduleUpdateRequest;
 import com.allog.dallog.domain.schedule.dto.response.ScheduleResponse;
-import com.allog.dallog.domain.subscription.domain.Subscription;
 import com.allog.dallog.domain.subscription.domain.SubscriptionRepository;
 import com.allog.dallog.domain.subscription.domain.Subscriptions;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -94,66 +86,5 @@ public class ScheduleService {
         Schedule schedule = scheduleRepository.getById(id);
         schedule.validateEditPossible(memberId);
         scheduleRepository.deleteById(id);
-    }
-
-    public AvailablePeriodMaterial findInMembersByCategoryIdAndDateRange(final Long categoryId,
-                                                                         final DateRangeRequest request) {
-        List<Long> memberIds = toMemberIds(categoryId);
-        List<Subscription> subscriptions = subscriptionRepository.findByMemberIdIn(memberIds);
-
-        List<IntegrationSchedule> internalSchedules = toInternalSchedules(subscriptions, request);
-        Map<String, List<ExternalRequestMaterial>> tokenByExternalIds = toTokenByExternalIds(subscriptions);
-
-        return new AvailablePeriodMaterial(internalSchedules, tokenByExternalIds);
-    }
-
-    private List<Long> toMemberIds(final Long categoryId) {
-        return subscriptionRepository.findByCategoryId(categoryId)
-                .stream()
-                .map(Subscription::getMember)
-                .map(Member::getId)
-                .collect(Collectors.toList());
-    }
-
-    private List<IntegrationSchedule> toInternalSchedules(final List<Subscription> subscriptions,
-                                                          final DateRangeRequest request) {
-        List<Category> internalCategories = toInternalCategories(subscriptions);
-
-        LocalDateTime startDate = request.getStartDateTime();
-        LocalDateTime endDate = request.getEndDateTime();
-        return scheduleRepository.getByCategoriesAndBetween(internalCategories, startDate, endDate);
-    }
-
-    private List<Category> toInternalCategories(final List<Subscription> subscriptions) {
-        return subscriptions.stream()
-                .filter(Subscription::hasInternalCategory)
-                .map(Subscription::getCategory)
-                .collect(Collectors.toList());
-    }
-
-    private Map<String, List<ExternalRequestMaterial>> toTokenByExternalIds(final List<Subscription> subscriptions) {
-        return subscriptions.stream()
-                .filter(Subscription::hasExternalCategory)
-                .map(this::toExternalRequestMaterial)
-                .collect(Collectors.groupingBy(ExternalRequestMaterial::getRefreshToken));
-    }
-
-    private ExternalRequestMaterial toExternalRequestMaterial(final Subscription subscription) {
-        String refreshToken = toRefreshToken(subscription);
-        Category category = subscription.getCategory();
-        String externalId = toExternalId(category);
-        return new ExternalRequestMaterial(refreshToken, category.getId(), externalId);
-    }
-
-
-    private String toRefreshToken(final Subscription subscription) {
-        Member member = subscription.getMember();
-        OAuthToken oAuthToken = oAuthTokenRepository.getByMemberId(member.getId());
-        return oAuthToken.getRefreshToken();
-    }
-
-    private String toExternalId(final Category category) {
-        ExternalCategoryDetail externalCategoryDetail = externalCategoryDetailRepository.getByCategory(category);
-        return externalCategoryDetail.getExternalId();
     }
 }
--- backend/src/test/java/com/allog/dallog/domain/schedule/application/ScheduleServiceTest.java
+++ backend/src/test/java/com/allog/dallog/domain/schedule/application/ScheduleServiceTest.java
@@ -1,30 +1,13 @@
 package com.allog.dallog.domain.schedule.application;
 
-import static com.allog.dallog.common.fixtures.AuthFixtures.관리자_인증_코드_토큰_요청;
 import static com.allog.dallog.common.fixtures.AuthFixtures.리버_인증_코드_토큰_요청;
-import static com.allog.dallog.common.fixtures.AuthFixtures.매트_인증_코드_토큰_요청;
-import static com.allog.dallog.common.fixtures.AuthFixtures.파랑_인증_코드_토큰_요청;
 import static com.allog.dallog.common.fixtures.AuthFixtures.후디_인증_코드_토큰_요청;
 import static com.allog.dallog.common.fixtures.CategoryFixtures.BE_일정_생성_요청;
 import static com.allog.dallog.common.fixtures.CategoryFixtures.FE_일정_생성_요청;
-import static com.allog.dallog.common.fixtures.CategoryFixtures.공통_일정_생성_요청;
 import static com.allog.dallog.common.fixtures.ExternalCategoryFixtures.대한민국_공휴일_생성_요청;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_10일_0시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_10일_11시_59분;
 import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_15일_16시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_16일_16시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_16일_16시_1분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_16일_18시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_16일_20시_0분;
 import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_1일_0시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_20일_0시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_20일_11시_59분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_27일_0시_0분;
 import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_31일_0시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_7월_7일_16시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_8월_15일_14시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_8월_15일_17시_0분;
-import static com.allog.dallog.common.fixtures.ScheduleFixtures.날짜_2022년_8월_15일_23시_59분;
 import static com.allog.dallog.common.fixtures.ScheduleFixtures.레벨_인터뷰_메모;
 import static com.allog.dallog.common.fixtures.ScheduleFixtures.레벨_인터뷰_시작일시;
 import static com.allog.dallog.common.fixtures.ScheduleFixtures.레벨_인터뷰_제목;
@@ -46,7 +29,6 @@ import com.allog.dallog.domain.category.application.CategoryService;
 import com.allog.dallog.domain.category.dto.response.CategoryResponse;
 import com.allog.dallog.domain.category.exception.NoSuchCategoryException;
 import com.allog.dallog.domain.schedule.domain.IntegrationSchedule;
-import com.allog.dallog.domain.schedule.dto.AvailablePeriodMaterial;
 import com.allog.dallog.domain.schedule.dto.request.DateRangeRequest;
 import com.allog.dallog.domain.schedule.dto.request.ScheduleCreateRequest;
 import com.allog.dallog.domain.schedule.dto.request.ScheduleUpdateRequest;
@@ -368,90 +350,5 @@ class ScheduleServiceTest extends ServiceTest {
         // when & then
         assertThatThrownBy(() -> scheduleService.delete(알록달록_회의.getId() + 1, 후디_id))
                 .isInstanceOf(NoSuchScheduleException.class);
-    }
-
-    @DisplayName("카테고리를 구독하는 유저들의 모든 내부 일정을 가져온다.")
-    @Test
-    void 카테고리를_구독하는_유저들의_모든_내부_일정을_가져온다() {
-        // given
-        Long 관리자_id = parseMemberId(관리자_인증_코드_토큰_요청());
-        CategoryResponse 공통_일정 = categoryService.save(관리자_id, 공통_일정_생성_요청);
-        scheduleService.save(관리자_id, 공통_일정.getId(),
-                new ScheduleCreateRequest("공통 일정 1", 날짜_2022년_7월_7일_16시_0분, 날짜_2022년_7월_10일_0시_0분, ""));
-        scheduleService.save(관리자_id, 공통_일정.getId(),
-                new ScheduleCreateRequest("공통 일정 2", 날짜_2022년_7월_10일_11시_59분, 날짜_2022년_7월_15일_16시_0분, ""));
-        scheduleService.save(관리자_id, 공통_일정.getId(),
-                new ScheduleCreateRequest("공통 일정 3", 날짜_2022년_7월_16일_16시_0분, 날짜_2022년_7월_16일_16시_1분, ""));
-
-        Long 매트_id = parseMemberId(매트_인증_코드_토큰_요청());
-        CategoryResponse BE_일정 = categoryService.save(매트_id, BE_일정_생성_요청);
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 1", 날짜_2022년_7월_16일_18시_0분, 날짜_2022년_7월_16일_20시_0분, ""));
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 2", 날짜_2022년_7월_16일_20시_0분, 날짜_2022년_7월_20일_0시_0분, ""));
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 3", 날짜_2022년_7월_20일_11시_59분, 날짜_2022년_7월_27일_0시_0분, ""));
-
-        Long 리버_id = parseMemberId(리버_인증_코드_토큰_요청());
-        Long 파랑_id = parseMemberId(파랑_인증_코드_토큰_요청());
-        Long 후디_id = parseMemberId(후디_인증_코드_토큰_요청());
-
-        subscriptionService.save(매트_id, 공통_일정.getId());
-        subscriptionService.save(리버_id, 공통_일정.getId());
-        subscriptionService.save(파랑_id, 공통_일정.getId());
-        subscriptionService.save(후디_id, 공통_일정.getId());
-
-        DateRangeRequest request = new DateRangeRequest("2022-07-07T16:00", "2022-08-15T12:00");
-
-        // when
-        AvailablePeriodMaterial actual = scheduleService.findInMembersByCategoryIdAndDateRange(공통_일정.getId(), request);
-
-        // then
-        assertThat(actual.getInternalSchedules()).hasSize(6);
-    }
-
-    @DisplayName("카테고리를 구독하는 유저들의 특정 구간의 내부 일정을 가져온다.")
-    @Test
-    void 카테고리를_구독하는_유저들의_특정_구간의_내부_일정을_가져온다() {
-        // given
-        Long 관리자_id = parseMemberId(관리자_인증_코드_토큰_요청());
-        CategoryResponse 공통_일정 = categoryService.save(관리자_id, 공통_일정_생성_요청);
-        scheduleService.save(관리자_id, 공통_일정.getId(),
-                new ScheduleCreateRequest("공통 일정 1", 날짜_2022년_7월_7일_16시_0분, 날짜_2022년_7월_10일_0시_0분, ""));
-        scheduleService.save(관리자_id, 공통_일정.getId(),
-                new ScheduleCreateRequest("공통 일정 2", 날짜_2022년_7월_10일_11시_59분, 날짜_2022년_7월_15일_16시_0분, ""));
-        scheduleService.save(관리자_id, 공통_일정.getId(),
-                new ScheduleCreateRequest("공통 일정 3", 날짜_2022년_7월_16일_16시_0분, 날짜_2022년_7월_16일_16시_1분, ""));
-
-        Long 매트_id = parseMemberId(매트_인증_코드_토큰_요청());
-
-        CategoryResponse BE_일정 = categoryService.save(매트_id, BE_일정_생성_요청);
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 1", 날짜_2022년_7월_16일_18시_0분, 날짜_2022년_7월_16일_20시_0분, ""));
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 2", 날짜_2022년_7월_16일_20시_0분, 날짜_2022년_7월_20일_0시_0분, ""));
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 3", 날짜_2022년_7월_20일_11시_59분, 날짜_2022년_7월_27일_0시_0분, ""));
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 4", 날짜_2022년_8월_15일_14시_0분, 날짜_2022년_8월_15일_17시_0분, ""));
-        scheduleService.save(매트_id, BE_일정.getId(),
-                new ScheduleCreateRequest("백엔드 일정 5", 날짜_2022년_8월_15일_14시_0분, 날짜_2022년_8월_15일_23시_59분, ""));
-
-        Long 리버_id = parseMemberId(리버_인증_코드_토큰_요청());
-        Long 파랑_id = parseMemberId(파랑_인증_코드_토큰_요청());
-        Long 후디_id = parseMemberId(후디_인증_코드_토큰_요청());
-
-        subscriptionService.save(매트_id, 공통_일정.getId());
-        subscriptionService.save(리버_id, 공통_일정.getId());
-        subscriptionService.save(파랑_id, 공통_일정.getId());
-        subscriptionService.save(후디_id, 공통_일정.getId());
-
-        DateRangeRequest request = new DateRangeRequest("2022-07-07T16:00", "2022-08-15T12:00");
-
-        // when
-        AvailablePeriodMaterial actual = scheduleService.findInMembersByCategoryIdAndDateRange(공통_일정.getId(), request);
-
-        // then
-        assertThat(actual.getInternalSchedules()).hasSize(6);
     }
 }
