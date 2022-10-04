047ab5b20a0bdfb91ef15d4913fa0d8daf4469b4
jhy979 <jhy979@gmail.com>
2022-10-04 16:11:36 +0900

    feat: 일정 관련 api 커스텀훅으로 변경

---
+++ frontend/src/hooks/@queries/schedules.ts
@@ -0,0 +1,102 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useRecoilValue } from 'recoil';
+
+import { ScheduleResponseType, ScheduleType } from '@/@types/schedule';
+
+import { userState } from '@/recoil/atoms';
+
+import { CACHE_KEY } from '@/constants/api';
+
+import scheduleApi from '@/api/schedule';
+
+interface useGetSchedulesProps {
+  startDateTime: string;
+  endDateTime: string;
+}
+
+interface useDeleteScheduleProps {
+  scheduleId: string;
+  onSuccess?: () => void;
+}
+
+interface usePatchScheduleProps {
+  scheduleId: string;
+  onSuccess?: () => void;
+}
+
+interface usePostScheduleProps {
+  categoryId: string;
+  onSuccess?: () => void;
+}
+
+function useDeleteSchedule({ scheduleId, onSuccess }: useDeleteScheduleProps) {
+  const { accessToken } = useRecoilValue(userState);
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation<AxiosResponse, AxiosError>(
+    () => scheduleApi.delete(accessToken, scheduleId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([CACHE_KEY.SCHEDULES]);
+        onSuccess && onSuccess();
+      },
+    }
+  );
+
+  return { mutate };
+}
+
+function useGetSchedules({ startDateTime, endDateTime }: useGetSchedulesProps) {
+  const { accessToken } = useRecoilValue(userState);
+
+  const { isLoading, data } = useQuery<AxiosResponse<ScheduleResponseType>, AxiosError>(
+    [CACHE_KEY.SCHEDULES],
+    () => scheduleApi.get(accessToken, startDateTime, endDateTime)
+  );
+
+  return {
+    isLoading,
+    data,
+  };
+}
+
+function usePatchSchedule({ scheduleId, onSuccess }: usePatchScheduleProps) {
+  const { accessToken } = useRecoilValue(userState);
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation<
+    AxiosResponse,
+    AxiosError,
+    Omit<ScheduleType, 'id' | 'categoryId' | 'colorCode' | 'categoryType'>,
+    unknown
+  >(CACHE_KEY.SCHEDULE, (body) => scheduleApi.patch(accessToken, scheduleId, body), {
+    onSuccess: () => {
+      queryClient.invalidateQueries([CACHE_KEY.SCHEDULES]);
+      onSuccess && onSuccess();
+    },
+  });
+
+  return { mutate };
+}
+
+function usePostSchedule({ categoryId, onSuccess }: usePostScheduleProps) {
+  const { accessToken } = useRecoilValue(userState);
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation<
+    AxiosResponse<{ schedules: ScheduleType[] }>,
+    AxiosError,
+    Omit<ScheduleType, 'id' | 'categoryId' | 'colorCode' | 'categoryType'>,
+    unknown
+  >((body) => scheduleApi.post(accessToken, Number(categoryId), body), {
+    onSuccess: () => {
+      queryClient.invalidateQueries([CACHE_KEY.SCHEDULES]);
+      onSuccess && onSuccess();
+    },
+  });
+
+  return { mutate };
+}
+
+export { useDeleteSchedule, useGetSchedules, usePatchSchedule, usePostSchedule };
