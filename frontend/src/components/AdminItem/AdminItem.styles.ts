04b0db25a6aa5e28625041a637e8f0a0b6996961
jhy979 <jhy979@gmail.com>
2022-10-07 15:51:16 +0900

    feat: 관리자 목록의 아이템 컴포넌트 구현

---
+++ frontend/src/components/AdminItem/AdminItem.styles.ts
@@ -0,0 +1,39 @@
+import { css, Theme } from '@emotion/react';
+
+const adminButtonStyle = css`
+  position: absolute;
+  right: 1rem;
+
+  font-size: 5rem;
+  line-height: 7rem;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const adminItemStyle = ({ colors, flex }: Theme) => css`
+  ${flex.row};
+
+  justify-content: flex-start;
+  gap: 2rem;
+  position: relative;
+
+  height: 7rem;
+  padding: 6rem 2rem;
+  box-shadow: 0px 2px 2px ${colors.GRAY_400};
+
+  font-size: 4rem;
+
+  &:hover {
+    background: ${colors.GRAY_100};
+  }
+`;
+
+const profileImageStyle = css`
+  width: 7rem;
+  height: 7rem;
+  border-radius: 50%;
+`;
+
+export { adminButtonStyle, adminItemStyle, profileImageStyle };
---
+++ frontend/src/components/AdminItem/AdminItem.tsx
@@ -0,0 +1,50 @@
+import { useRecoilValue } from 'recoil';
+
+import { usePatchCategoryRole } from '@/hooks/@queries/category';
+
+import { ProfileType } from '@/@types/profile';
+
+import { userState } from '@/recoil/atoms';
+
+import Button from '@/components/@common/Button/Button';
+
+import { CONFIRM_MESSAGE } from '@/constants/message';
+
+import { MdPersonOff } from 'react-icons/md';
+
+import { adminButtonStyle, adminItemStyle, profileImageStyle } from './AdminItem.styles';
+
+interface AdminItemProps {
+  categoryId: number;
+  admin: ProfileType;
+}
+
+function AdminItem({ categoryId, admin }: AdminItemProps) {
+  const { id } = useRecoilValue(userState);
+
+  const { mutate: patchRole } = usePatchCategoryRole({
+    categoryId,
+    memberId: admin.id,
+  });
+
+  const handleClickDeleteRoleButton = () => {
+    window.confirm(CONFIRM_MESSAGE.DELETE_ADMIN) &&
+      patchRole({
+        categoryRoleType: 'NONE',
+      });
+  };
+
+  return (
+    <div key={admin.id} css={adminItemStyle}>
+      <img src={admin.profileImageUrl} alt="프로필 이미지" css={profileImageStyle} />
+      <span>{admin.displayName}</span>
+      {admin.id !== id && (
+        <Button cssProp={adminButtonStyle} onClick={handleClickDeleteRoleButton}>
+          <MdPersonOff />
+        </Button>
+      )}
+    </div>
+  );
+}
+
+export default AdminItem;
