4412947d42274e7a8adb5fd40b7f650dc21d8e7b
jhy979 <jhy979@gmail.com>
2022-10-07 14:37:41 +0900

    feat: 새롭게 추가된 카테고리 api 구현
    - 편집 권한이 있는 카테고리 가져오기
    - Admin 권한이 있는 카테고리 가져오기
    - 카테고리 구독자 목록 가져오기
    - 역할 수정하기

--- frontend/src/api/category.ts
+++ frontend/src/api/category.ts
@@ -1,16 +1,44 @@
-import { CategoriesGetResponseType, CategoryType } from '@/@types/category';
+import {
+  CategoriesGetResponseType,
+  CategoryRoleType,
+  CategorySubscriberType,
+  CategoryType,
+} from '@/@types/category';
 
 import dallogApi from './';
 
 const categoryApi = {
   endpoint: {
+    admin: '/api/categories/me/admin',
+    editable: '/api/categories/me/schedule-editable',
     entire: '/api/categories',
     my: '/api/categories/me',
     schedules: (categoryId: number) => `/api/categories/${categoryId}/schedules`,
+    subscribers: (categoryId: number) => `/api/categories/${categoryId}/subscribers`,
+    role: (categoryId: number, memberId: number) =>
+      `/api/categories/${categoryId}/subscribers/${memberId}/role`,
   },
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
+  },
+
+  getAdmin: async (accessToken: string) => {
+    const response = await dallogApi.get(categoryApi.endpoint.admin, {
+      headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
+      transformResponse: (res) => JSON.parse(res).categories,
+    });
+
+    return response;
+  },
+
+  getEditable: async (accessToken: string) => {
+    const response = await dallogApi.get(categoryApi.endpoint.editable, {
+      headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
+      transformResponse: (res) => JSON.parse(res).categories,
+    });
+
+    return response;
   },
 
   getEntire: async (name: string, page: number, size: number) => {
@@ -34,6 +62,18 @@ const categoryApi = {
     const response = await dallogApi.get(`${categoryApi.endpoint.entire}/${categoryId}`, {
       headers: { ...categoryApi.headers },
     });
+
+    return response;
+  },
+
+  getSubscribers: async (accessToken: string, categoryId: number) => {
+    const response = await dallogApi.get<CategorySubscriberType[]>(
+      categoryApi.endpoint.subscribers(categoryId),
+      {
+        headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
+        transformResponse: (res) => JSON.parse(res).subscribers,
+      }
+    );
 
     return response;
   },
@@ -70,6 +110,19 @@ const categoryApi = {
 
   patch: async (accessToken: string, categoryId: number, body: Pick<CategoryType, 'name'>) => {
     const response = await dallogApi.patch(`${categoryApi.endpoint.entire}/${categoryId}`, body, {
+      headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
+    });
+
+    return response;
+  },
+
+  patchRole: async (
+    accessToken: string,
+    categoryId: number,
+    memberId: number,
+    body: { categoryRoleType: CategoryRoleType }
+  ) => {
+    const response = await dallogApi.patch(categoryApi.endpoint.role(categoryId, memberId), body, {
       headers: { ...categoryApi.headers, Authorization: `Bearer ${accessToken}` },
     });
 
