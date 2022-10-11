c969a0f382ad042f1db73cd5efc383b0964cabf5
hyeonic <dev.hyeonic@gmail.com>
2022-10-11 16:08:43 +0900

    feat: 외부 요청 sleep 처리

--- backend/src/main/java/com/allog/dallog/infrastructure/oauth/client/GoogleExternalCalendarClient.java
+++ backend/src/main/java/com/allog/dallog/infrastructure/oauth/client/GoogleExternalCalendarClient.java
@@ -3,20 +3,10 @@ package com.allog.dallog.infrastructure.oauth.client;
 import com.allog.dallog.domain.externalcalendar.application.ExternalCalendarClient;
 import com.allog.dallog.domain.externalcalendar.dto.ExternalCalendar;
 import com.allog.dallog.domain.schedule.domain.IntegrationSchedule;
-import com.allog.dallog.infrastructure.oauth.dto.GoogleCalendarEventsResponse;
-import com.allog.dallog.infrastructure.oauth.dto.GoogleCalendarListResponse;
-import com.allog.dallog.infrastructure.oauth.exception.OAuthException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -34,22 +24,12 @@ public class GoogleExternalCalendarClient implements ExternalCalendarClient {
 
     @Override
     public List<ExternalCalendar> getExternalCalendars(final String accessToken) {
-        HttpEntity<Void> request = new HttpEntity<>(generateCalendarRequestHeaders(accessToken));
-        GoogleCalendarListResponse response = fetchGoogleCalendarList(request).getBody();
-
-        return response.getItems()
-                .stream()
-                .map(item -> new ExternalCalendar(item.getId(), item.getSummary()))
-                .collect(Collectors.toList());
-    }
-
-    private ResponseEntity<GoogleCalendarListResponse> fetchGoogleCalendarList(final HttpEntity<Void> request) {
         try {
-            return restTemplate.exchange(CALENDAR_LIST_REQUEST_URI, HttpMethod.GET, request,
-                    GoogleCalendarListResponse.class);
-        } catch (RestClientException e) {
-            throw new OAuthException(e);
+            Thread.sleep(1);
+        } catch (final InterruptedException e) {
         }
+
+        return new ArrayList<>();
     }
 
     @Override
@@ -58,40 +38,11 @@ public class GoogleExternalCalendarClient implements ExternalCalendarClient {
                                                                   final String externalCalendarId,
                                                                   final String startDateTime,
                                                                   final String endDateTime) {
-        HttpEntity<Void> request = new HttpEntity<>(generateCalendarRequestHeaders(accessToken));
-        Map<String, String> uriVariables = generateEventsVariables(externalCalendarId, startDateTime, endDateTime);
-
-        GoogleCalendarEventsResponse response = fetchGoogleCalendarEvents(request, uriVariables).getBody();
-
-        return response.getItems()
-                .stream()
-                .map(event -> event.toIntegrationSchedule(internalCategoryId))
-                .collect(Collectors.toList());
-    }
-
-    private HttpHeaders generateCalendarRequestHeaders(final String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-        headers.set(ACCEPT_HEADER_NAME, MediaType.APPLICATION_JSON_VALUE);
-        return headers;
-    }
-
-    private Map<String, String> generateEventsVariables(final String externalCalendarId, final String startDateTime,
-                                                        final String endDateTime) {
-        return Map.of(
-                "calendarId", externalCalendarId,
-                "timeMax", endDateTime + "Z",
-                "timeMin", startDateTime + "Z"
-        );
-    }
-
-    private ResponseEntity<GoogleCalendarEventsResponse> fetchGoogleCalendarEvents(
-            final HttpEntity<Void> request, final Map<String, String> uriVariables) {
         try {
-            return restTemplate.exchange(CALENDAR_EVENTS_REQUEST_URI, HttpMethod.GET, request,
-                    GoogleCalendarEventsResponse.class, uriVariables);
-        } catch (RestClientException e) {
-            throw new OAuthException(e);
+            Thread.sleep(1);
+        } catch (final InterruptedException e) {
         }
+
+        return new ArrayList<>();
     }
 }
