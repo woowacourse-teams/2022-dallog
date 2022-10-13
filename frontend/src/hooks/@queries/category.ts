c53d6b9ef7899ffcec9237679b677817ef12b710
jhy979 <jhy979@gmail.com>
2022-10-13 20:11:12 +0900

    fix: 권한 포기시 쿼리 무효화 추가

--- frontend/src/hooks/@queries/category.ts
+++ frontend/src/hooks/@queries/category.ts
@@ -224,6 +224,8 @@ function usePatchCategoryRole({ categoryId, memberId, onSuccess }: UsePatchCateg
   >((body) => categoryApi.patchRole(accessToken, categoryId, memberId, body), {
     onSuccess: () => {
       queryClient.invalidateQueries([CACHE_KEY.SUBSCRIBERS]);
+      queryClient.invalidateQueries([CACHE_KEY.EDITABLE_CATEGORIES]);
+      queryClient.invalidateQueries([CACHE_KEY.SUBSCRIPTIONS]);
       onSuccess && onSuccess();
     },
   });
