a3ac59fde6a6323bec44def6571c21a153a7577c
jhy979 <jhy979@gmail.com>
2022-10-16 10:06:37 +0900

    refactor: 프로필 수정 시 제어컴포넌트로 변경

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,21 +1,20 @@
-import { useRef, useState } from 'react';
+import { validateLength } from '@/validation';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { usePatchProfile } from '@/hooks/@queries/profile';
-import useToggle from '@/hooks/useToggle';
+import useControlledInput from '@/hooks/useControlledInput';
 
 import { userState } from '@/recoil/atoms';
 
 import Button from '@/components/@common/Button/Button';
 import Fieldset from '@/components/@common/Fieldset/Fieldset';
-import ModalPortal from '@/components/@common/ModalPortal/ModalPortal';
-import WithdrawalModal from '@/components/WithdrawalModal/WithdrawalModal';
 
 import { PATH } from '@/constants';
 import { CONFIRM_MESSAGE } from '@/constants/message';
+import { VALIDATION_MESSAGE, VALIDATION_SIZE } from '@/constants/validate';
 
-import { createPostBody } from '@/utils';
 import { removeAccessToken, removeRefreshToken } from '@/utils/storage';
 
 import { MdOutlineCheck, MdOutlineModeEdit } from 'react-icons/md';
@@ -40,9 +39,7 @@ function Profile() {
 
   const [isEditingName, setEditingName] = useState(false);
 
-  const inputRef = {
-    displayName: useRef<HTMLInputElement>(null),
-  };
+  const editDisplayName = useControlledInput(user.displayName);
 
   const { mutate } = usePatchProfile({ accessToken: user.accessToken });
 
@@ -50,18 +47,11 @@ function Profile() {
     setEditingName(true);
   };
 
-  const handleClickCompleteButton = (defaultName: string | undefined) => {
-    if (defaultName === undefined) {
-      return;
-    }
+  const handleClickCompleteButton = () => {
+    mutate({
+      displayName: editDisplayName.inputValue,
+    });
 
-    const body = createPostBody(inputRef);
-
-    if (body.displayName === '') {
-      body.displayName = defaultName;
-    }
-
-    mutate(body);
     setEditingName(false);
   };
 
@@ -83,14 +73,31 @@ function Profile() {
             <Fieldset
               defaultValue={user.displayName}
               placeholder={user.displayName}
-              refProp={inputRef.displayName}
+              value={editDisplayName.inputValue}
+              onChange={editDisplayName.onChangeValue}
               cssProp={inputStyle}
               autoFocus={true}
+              isValid={validateLength(
+                editDisplayName.inputValue,
+                VALIDATION_SIZE.MIN_LENGTH,
+                VALIDATION_SIZE.DISPLAY_NAME_MAX_LENGTH
+              )}
+              errorMessage={VALIDATION_MESSAGE.STRING_LENGTH(
+                VALIDATION_SIZE.MIN_LENGTH,
+                VALIDATION_SIZE.DISPLAY_NAME_MAX_LENGTH
+              )}
             />
             <Button
               type="submit"
               cssProp={menu}
-              onClick={() => handleClickCompleteButton(user.displayName)}
+              onClick={handleClickCompleteButton}
+              disabled={
+                !validateLength(
+                  editDisplayName.inputValue,
+                  VALIDATION_SIZE.MIN_LENGTH,
+                  VALIDATION_SIZE.DISPLAY_NAME_MAX_LENGTH
+                )
+              }
             >
               <MdOutlineCheck size={14} />
               <span css={menuTitle}>완료</span>
