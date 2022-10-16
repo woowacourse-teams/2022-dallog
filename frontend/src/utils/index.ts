999c7f5e713d4a099312dbe9b00dc31cef9a5db6
jhy979 <jhy979@gmail.com>
2022-10-16 10:06:53 +0900

    refactor: 불필요한 유틸함수 제거

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,24 +1,3 @@
-import { InputRefType } from '@/@types';
-
-const createPostBody = (inputRef: InputRefType) => {
-  const inputElements = Object.values(inputRef).map((el) => el.current);
-  const isValidInputRefs = inputElements.every((el) => el instanceof HTMLInputElement);
-
-  if (!isValidInputRefs) {
-    return;
-  }
-
-  const inputRefKeys = Object.keys(inputRef);
-
-  const body = inputRefKeys.reduce((acc: any, cur: number | string) => {
-    acc[cur] = (inputRef[cur].current as HTMLInputElement).value;
-
-    return acc;
-  }, {});
-
-  return body;
-};
-
 const getRandomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
@@ -31,4 +10,4 @@ const zeroFill = (str: string | number) => {
   return str.toString().padStart(2, '0');
 };
 
-export { createPostBody, getRandomNumber, getSearchParam, zeroFill };
+export { getRandomNumber, getSearchParam, zeroFill };
