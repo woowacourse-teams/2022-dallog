acd50dcecfd8bd62bd1b17ad0391b2aaaafcc485
hyeonic <dev.hyeonic@gmail.com>
2022-10-17 18:32:18 +0900

    feat: sleep 시간 조정

--- backend/src/main/java/com/allog/dallog/infrastructure/oauth/client/GoogleExternalCalendarClient.java
+++ backend/src/main/java/com/allog/dallog/infrastructure/oauth/client/GoogleExternalCalendarClient.java
@@ -5,27 +5,15 @@ import com.allog.dallog.domain.externalcalendar.dto.ExternalCalendar;
 import com.allog.dallog.domain.schedule.domain.IntegrationSchedule;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class GoogleExternalCalendarClient implements ExternalCalendarClient {
 
-    private static final String CALENDAR_LIST_REQUEST_URI = "https://www.googleapis.com/calendar/v3/users/me/calendarList";
-    private static final String CALENDAR_EVENTS_REQUEST_URI = "https://www.googleapis.com/calendar/v3/calendars/{calendarId}/events?singleEvents=true&timeMax={timeMax}&timeMin={timeMin}";
-    private static final String ACCEPT_HEADER_NAME = "Accept";
-
-    private final RestTemplate restTemplate;
-
-    public GoogleExternalCalendarClient(final RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
     @Override
     public List<ExternalCalendar> getExternalCalendars(final String accessToken) {
         try {
-            Thread.sleep(1);
+            Thread.sleep(150);
         } catch (final InterruptedException e) {
         }
 
@@ -39,7 +27,7 @@ public class GoogleExternalCalendarClient implements ExternalCalendarClient {
                                                                   final String startDateTime,
                                                                   final String endDateTime) {
         try {
-            Thread.sleep(1);
+            Thread.sleep(150);
         } catch (final InterruptedException e) {
         }
 
