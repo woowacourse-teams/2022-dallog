51ac2716671bc1b71beedf50af8e612f8953a65a
devHudi <devhudi@gmail.com>
2022-10-18 17:00:06 +0900

    uri

--- backend/src/main/java/com/allog/dallog/infrastructure/oauth/uri/DevGoogleOAuthUri.java
+++ backend/src/main/java/com/allog/dallog/infrastructure/oauth/uri/DevGoogleOAuthUri.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("dev")
+@Profile({"dev", "local"})
 public class DevGoogleOAuthUri implements OAuthUri {
 
     private final GoogleProperties properties;
