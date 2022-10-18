c9f06e1fc34c020309b0efd6240f757fc76f0749
devHudi <devhudi@gmail.com>
2022-10-18 16:55:13 +0900

    고침

--- backend/src/main/java/com/allog/dallog/global/config/replication/DataSourceConfiguration.java
+++ backend/src/main/java/com/allog/dallog/global/config/replication/DataSourceConfiguration.java
@@ -1,5 +1,12 @@
 package com.allog.dallog.global.config.replication;
 
+import static com.allog.dallog.global.config.replication.DataSourceKey.KeyName.REPLICA_1_NAME;
+import static com.allog.dallog.global.config.replication.DataSourceKey.KeyName.REPLICA_2_NAME;
+import static com.allog.dallog.global.config.replication.DataSourceKey.KeyName.SOURCE_NAME;
+import static com.allog.dallog.global.config.replication.DataSourceKey.REPLICA_1;
+import static com.allog.dallog.global.config.replication.DataSourceKey.REPLICA_2;
+import static com.allog.dallog.global.config.replication.DataSourceKey.SOURCE;
+
 import java.util.HashMap;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,11 +22,8 @@ import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 @Profile({"prod", "dev"})
 public class DataSourceConfiguration {
 
-    private static final String SOURCE_SERVER = "SOURCE";
-    private static final String REPLICA_SERVER = "REPLICA";
-
     @Bean
-    @Qualifier(SOURCE_SERVER)
+    @Qualifier(SOURCE_NAME)
     @ConfigurationProperties(prefix = "spring.datasource.source")
     public DataSource sourceDataSource() {
         return DataSourceBuilder.create()
@@ -27,23 +31,33 @@ public class DataSourceConfiguration {
     }
 
     @Bean
-    @Qualifier(REPLICA_SERVER)
-    @ConfigurationProperties(prefix = "spring.datasource.replica")
-    public DataSource replicaDataSource() {
+    @Qualifier(REPLICA_1_NAME)
+    @ConfigurationProperties(prefix = "spring.datasource.replica1")
+    public DataSource replica1DataSource() {
+        return DataSourceBuilder.create()
+                .build();
+    }
+
+    @Bean
+    @Qualifier(REPLICA_2_NAME)
+    @ConfigurationProperties(prefix = "spring.datasource.replica2")
+    public DataSource replica2DataSource() {
         return DataSourceBuilder.create()
                 .build();
     }
 
     @Bean
     public DataSource routingDataSource(
-            @Qualifier(SOURCE_SERVER) DataSource sourceDataSource,
-            @Qualifier(REPLICA_SERVER) DataSource replicaDataSource
+            @Qualifier(SOURCE_NAME) DataSource sourceDataSource,
+            @Qualifier(REPLICA_1_NAME) DataSource replica1DataSource,
+            @Qualifier(REPLICA_2_NAME) DataSource replica2DataSource
     ) {
         RoutingDataSource routingDataSource = new RoutingDataSource();
 
         HashMap<Object, Object> dataSourceMap = new HashMap<>();
-        dataSourceMap.put(SOURCE_SERVER, sourceDataSource);
-        dataSourceMap.put(REPLICA_SERVER, replicaDataSource);
+        dataSourceMap.put(SOURCE, sourceDataSource);
+        dataSourceMap.put(REPLICA_1, replica1DataSource);
+        dataSourceMap.put(REPLICA_2, replica2DataSource);
 
         routingDataSource.setTargetDataSources(dataSourceMap);
         routingDataSource.setDefaultTargetDataSource(sourceDataSource);
@@ -54,7 +68,8 @@ public class DataSourceConfiguration {
     @Bean
     @Primary
     public DataSource dataSource() {
-        DataSource determinedDataSource = routingDataSource(sourceDataSource(), replicaDataSource());
+        DataSource determinedDataSource = routingDataSource(sourceDataSource(), replica1DataSource(),
+                replica2DataSource());
         return new LazyConnectionDataSourceProxy(determinedDataSource);
     }
 }
