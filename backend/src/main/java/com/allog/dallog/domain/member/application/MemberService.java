f1a4b9b8275b8e25d1c96da811a3cc6c96a41711
koo <gudonghee2000@gmail.com>
2022-10-19 15:13:47 +0900

    chore: 코드 줄바꿈 정렬

--- backend/src/main/java/com/allog/dallog/domain/member/application/MemberService.java
+++ backend/src/main/java/com/allog/dallog/domain/member/application/MemberService.java
@@ -1,11 +1,14 @@
 package com.allog.dallog.domain.member.application;
 
-import static com.allog.dallog.domain.categoryrole.domain.CategoryAuthority.FIND_SUBSCRIBERS;
-import static com.allog.dallog.domain.categoryrole.domain.CategoryRoleType.ADMIN;
+import static com.allog.dallog.domain.categoryrole.domain.CategoryAuthority.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.allog.dallog.domain.auth.domain.OAuthTokenRepository;
 import com.allog.dallog.domain.auth.domain.TokenRepository;
-import com.allog.dallog.domain.category.domain.Category;
 import com.allog.dallog.domain.category.domain.CategoryRepository;
 import com.allog.dallog.domain.category.domain.ExternalCategoryDetailRepository;
 import com.allog.dallog.domain.categoryrole.domain.CategoryRole;
@@ -15,71 +18,64 @@ import com.allog.dallog.domain.member.domain.MemberRepository;
 import com.allog.dallog.domain.member.dto.request.MemberUpdateRequest;
 import com.allog.dallog.domain.member.dto.response.MemberResponse;
 import com.allog.dallog.domain.member.dto.response.SubscribersResponse;
-import com.allog.dallog.domain.member.exception.InvalidMemberException;
 import com.allog.dallog.domain.schedule.domain.ScheduleRepository;
 import com.allog.dallog.domain.subscription.domain.Subscription;
 import com.allog.dallog.domain.subscription.domain.SubscriptionRepository;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 @Service
 public class MemberService {
 
-    private final MemberRepository memberRepository;
-    private final CategoryRepository categoryRepository;
-    private final ExternalCategoryDetailRepository externalCategoryDetailRepository;
-    private final ScheduleRepository scheduleRepository;
-    private final SubscriptionRepository subscriptionRepository;
-    private final OAuthTokenRepository oAuthTokenRepository;
-    private final CategoryRoleRepository categoryRoleRepository;
-    private final TokenRepository tokenRepository;
+	private final MemberRepository memberRepository;
+	private final CategoryRepository categoryRepository;
+	private final ExternalCategoryDetailRepository externalCategoryDetailRepository;
+	private final ScheduleRepository scheduleRepository;
+	private final SubscriptionRepository subscriptionRepository;
+	private final OAuthTokenRepository oAuthTokenRepository;
+	private final CategoryRoleRepository categoryRoleRepository;
+	private final TokenRepository tokenRepository;
 
-    public MemberService(final MemberRepository memberRepository, final CategoryRepository categoryRepository,
-                         final ExternalCategoryDetailRepository externalCategoryDetailRepository,
-                         final ScheduleRepository scheduleRepository,
-                         final SubscriptionRepository subscriptionRepository,
-                         final OAuthTokenRepository oAuthTokenRepository,
-                         final CategoryRoleRepository categoryRoleRepository,
-                         final TokenRepository tokenRepository) {
-        this.memberRepository = memberRepository;
-        this.categoryRepository = categoryRepository;
-        this.externalCategoryDetailRepository = externalCategoryDetailRepository;
-        this.scheduleRepository = scheduleRepository;
-        this.subscriptionRepository = subscriptionRepository;
-        this.oAuthTokenRepository = oAuthTokenRepository;
-        this.categoryRoleRepository = categoryRoleRepository;
-        this.tokenRepository = tokenRepository;
-    }
+	public MemberService(final MemberRepository memberRepository, final CategoryRepository categoryRepository,
+		final ExternalCategoryDetailRepository externalCategoryDetailRepository,
+		final ScheduleRepository scheduleRepository, final SubscriptionRepository subscriptionRepository,
+		final OAuthTokenRepository oAuthTokenRepository, final CategoryRoleRepository categoryRoleRepository,
+		final TokenRepository tokenRepository) {
+		this.memberRepository = memberRepository;
+		this.categoryRepository = categoryRepository;
+		this.externalCategoryDetailRepository = externalCategoryDetailRepository;
+		this.scheduleRepository = scheduleRepository;
+		this.subscriptionRepository = subscriptionRepository;
+		this.oAuthTokenRepository = oAuthTokenRepository;
+		this.categoryRoleRepository = categoryRoleRepository;
+		this.tokenRepository = tokenRepository;
+	}
 
-    public MemberResponse findById(final Long id) {
-        return new MemberResponse(memberRepository.getById(id));
-    }
+	public MemberResponse findById(final Long id) {
+		return new MemberResponse(memberRepository.getById(id));
+	}
 
-    public MemberResponse findBySubscriptionId(final Long subscriptionId) {
-        Subscription subscription = subscriptionRepository.getById(subscriptionId);
+	public MemberResponse findBySubscriptionId(final Long subscriptionId) {
+		Subscription subscription = subscriptionRepository.getById(subscriptionId);
 
-        Member member = subscription.getMember();
-        return new MemberResponse(member);
-    }
+		Member member = subscription.getMember();
+		return new MemberResponse(member);
+	}
 
-    public SubscribersResponse findSubscribers(final Long loginMemberId, final Long categoryId) {
-        CategoryRole categoryRole = categoryRoleRepository.getByMemberIdAndCategoryId(loginMemberId, categoryId);
-        categoryRole.validateAuthority(FIND_SUBSCRIBERS);
+	public SubscribersResponse findSubscribers(final Long loginMemberId, final Long categoryId) {
+		CategoryRole categoryRole = categoryRoleRepository.getByMemberIdAndCategoryId(loginMemberId, categoryId);
+		categoryRole.validateAuthority(FIND_SUBSCRIBERS);
 
-        List<CategoryRole> categoryRoles = categoryRoleRepository.findByCategoryId(categoryId);
-        return new SubscribersResponse(categoryRoles);
-    }
+		List<CategoryRole> categoryRoles = categoryRoleRepository.findByCategoryId(categoryId);
+		return new SubscribersResponse(categoryRoles);
+	}
 
-    @Transactional
-    public void update(final Long id, final MemberUpdateRequest request) {
-        Member member = memberRepository.getById(id);
-        member.change(request.getDisplayName());
-    }
+	@Transactional
+	public void update(final Long id, final MemberUpdateRequest request) {
+		Member member = memberRepository.getById(id);
+		member.change(request.getDisplayName());
+	}
 
-    @Transactional
-    public void deleteById(final Long id) {
-    }
+	@Transactional
+	public void deleteById(final Long id) {
+	}
 }
