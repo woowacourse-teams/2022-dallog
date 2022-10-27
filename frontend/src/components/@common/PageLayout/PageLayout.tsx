99a774d217c7236194aeb5a18126292fca4f1069
jhy979 <jhy979@gmail.com>
2022-10-28 08:43:51 +0900

    style: 페이지 레이아웃 스타일 변경 및 타입 추가

--- frontend/src/components/@common/PageLayout/PageLayout.tsx
+++ frontend/src/components/@common/PageLayout/PageLayout.tsx
@@ -1,20 +1,25 @@
 import { useTheme } from '@emotion/react';
 import { useRecoilValue } from 'recoil';
 
+import { ValueOf } from '@/@types/util';
+
 import { sideBarState } from '@/recoil/atoms';
+
+import { PAGE_LAYOUT } from '@/constants/style';
 
 import { pageLayout } from './PageLayout.styles';
 
 interface PageLayoutProps {
+  type?: ValueOf<typeof PAGE_LAYOUT>;
   children: JSX.Element | JSX.Element[];
 }
 
-function PageLayout({ children }: PageLayoutProps) {
+function PageLayout({ type = PAGE_LAYOUT.DEFAULT, children }: PageLayoutProps) {
   const theme = useTheme();
 
   const isSideBarOpen = useRecoilValue(sideBarState);
 
-  return <div css={pageLayout(theme, isSideBarOpen)}>{children}</div>;
+  return <div css={pageLayout(theme, isSideBarOpen, type)}>{children}</div>;
 }
 
 export default PageLayout;
