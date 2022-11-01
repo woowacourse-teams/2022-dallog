1283ce9d977980119ac9efa26236a5105894d5d6
Daye Lee <508yeah@gmail.com>
2022-11-01 20:27:21 +0900

    refactor: 카테고리 페이지에 달력 컴포넌트 사용

--- frontend/src/pages/CategoryPage/CategoryPage.styles.ts
+++ frontend/src/pages/CategoryPage/CategoryPage.styles.ts
@@ -1,13 +1,12 @@
 import { css, Theme } from '@emotion/react';
 
-import { DAYS } from '@/constants/date';
-
 const categoryPageStyle = ({ flex }: Theme) => css`
   ${flex.row};
 
   justify-content: space-around;
+  align-items: flex-start;
 
-  padding: 5rem;
+  height: 100%;
 `;
 
 const calendarStyle = ({ mq }: Theme) => css`
@@ -20,149 +19,4 @@ const calendarStyle = ({ mq }: Theme) => css`
   }
 `;
 
-const hintStyle = ({ colors }: Theme) => css`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%);
-  z-index: 10;
-
-  padding: 4rem 6rem;
-  border-radius: 7px;
-
-  background: ${colors.ORANGE_500};
-
-  font-size: 4rem;
-  font-weight: 500;
-  color: ${colors.WHITE};
-`;
-
-const calendarHeaderStyle = ({ colors, flex }: Theme) => css`
-  ${flex.row}
-
-  justify-content: space-between;
-
-  width: 100%;
-  padding: 0 2rem 3rem;
-
-  font-size: 5rem;
-  font-weight: 500;
-  color: ${colors.GRAY_700};
-`;
-
-const waitingNavStyle = ({ flex }: Theme) => css`
-  ${flex.row}
-
-  gap: 4rem;
-`;
-
-const spinnerStyle = ({ flex }: Theme) => css`
-  ${flex.row}
-
-  gap: 2rem;
-
-  width: 100%;
-  height: 100%;
-
-  font-size: 3rem;
-`;
-
-const monthPickerStyle = ({ flex }: Theme) => css`
-  ${flex.row}
-
-  justify-content: space-around;
-`;
-
-const todayButtonStyle = ({ colors }: Theme) => css`
-  width: 15rem;
-  height: 8rem;
-
-  padding: auto 0;
-
-  font-size: 4rem;
-  line-height: 4rem;
-  font-weight: 500;
-  color: ${colors.GRAY_700};
-`;
-
-const navButtonStyle = ({ colors }: Theme) => css`
-  position: relative;
-
-  width: 8rem;
-  height: 8rem;
-  padding: 0;
-
-  font-size: 4rem;
-  line-height: 4rem;
-  color: ${colors.GRAY_600};
-
-  &:hover {
-    border-radius: 50%;
-
-    background: ${colors.GRAY_100};
-
-    filter: none;
-  }
-
-  &:hover span {
-    visibility: visible;
-  }
-`;
-
-const navButtonTitleStyle = ({ colors }: Theme) => css`
-  visibility: hidden;
-  position: absolute;
-
-  top: 120%;
-  left: 50%;
-  transform: translateX(-50%);
-
-  padding: 2rem 3rem;
-
-  background: ${colors.GRAY_700}ee;
-
-  font-size: 3rem;
-  font-weight: normal;
-  color: ${colors.WHITE};
-  white-space: nowrap;
-`;
-
-const dayBarGridStyle = css`
-  display: grid;
-  grid-template-columns: repeat(7, calc(100% / 7));
-
-  height: 7rem;
-`;
-
-const dayBarStyle = ({ colors }: Theme, day: string) => css`
-  padding: 2rem 3rem;
-  border-top: 1px solid ${colors.GRAY_300};
-  border-right: 1px solid ${colors.GRAY_300};
-  border-left: ${day === DAYS[0] && `1px solid ${colors.GRAY_300}`};
-
-  font-size: 3rem;
-  text-align: right;
-  color: ${day === DAYS[0] && colors.RED_400};
-`;
-
-const calendarGridStyle = (rowNum: number) => css`
-  display: grid;
-  grid-template-columns: repeat(7, calc(100% / 7));
-  grid-auto-rows: calc(calc(100vh - 44rem) / ${rowNum});
-`;
-
-export {
-  calendarGridStyle,
-  calendarHeaderStyle,
-  calendarStyle,
-  categoryPageStyle,
-  dayBarGridStyle,
-  dayBarStyle,
-  hintStyle,
-  monthPickerStyle,
-  navButtonStyle,
-  navButtonTitleStyle,
-  spinnerStyle,
-  todayButtonStyle,
-  waitingNavStyle,
-};
+export { calendarStyle, categoryPageStyle };
--- frontend/src/pages/CategoryPage/CategoryPage.tsx
+++ frontend/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,62 +1,24 @@
-import { useTheme } from '@emotion/react';
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { useGetSchedulesWithCategory } from '@/hooks/@queries/category';
 import useCalendar from '@/hooks/useCalendar';
-import useRootFontSize from '@/hooks/useRootFontSize';
 
 import { CategoryType } from '@/@types/category';
 
-import Button from '@/components/@common/Button/Button';
 import PageLayout from '@/components/@common/PageLayout/PageLayout';
-import Spinner from '@/components/@common/Spinner/Spinner';
+import Calendar from '@/components/Calendar/Calendar';
+import CalendarFallback from '@/components/Calendar/Calendar.fallback';
 import CategoryControl from '@/components/CategoryControl/CategoryControl';
-import DateCell from '@/components/DateCell/DateCell';
 
-import { DAYS } from '@/constants/date';
-import { PAGE_LAYOUT, SCHEDULE } from '@/constants/style';
+import { PAGE_LAYOUT } from '@/constants/style';
 
-import { extractDateTime } from '@/utils/date';
-
-import getSchedulePriority from '@/domains/schedule';
-
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
-
-import {
-  calendarGridStyle,
-  calendarHeaderStyle,
-  calendarStyle,
-  categoryPageStyle,
-  dayBarGridStyle,
-  dayBarStyle,
-  hintStyle,
-  monthPickerStyle,
-  navButtonStyle,
-  navButtonTitleStyle,
-  spinnerStyle,
-  todayButtonStyle,
-  waitingNavStyle,
-} from './CategoryPage.styles';
+import { calendarStyle, categoryPageStyle } from './CategoryPage.styles';
 
 function CategoryPage() {
-  const theme = useTheme();
-
-  const dateCellRef = useRef<HTMLDivElement>(null);
-
-  const [maxScheduleCount, setMaxScheduleCount] = useState(0);
   const [category, setCategory] = useState<Pick<CategoryType, 'id' | 'name'>>({ id: 0, name: '' });
 
-  const rootFontSize = useRootFontSize();
-
-  const {
-    calendar,
-    currentDateTime,
-    moveToBeforeMonth,
-    moveToToday,
-    moveToNextMonth,
-    startDateTime,
-    endDateTime,
-  } = useCalendar();
+  const calendarController = useCalendar();
+  const { startDateTime, endDateTime } = calendarController;
 
   const { isLoading, data } = useGetSchedulesWithCategory({
     categoryId: category.id,
@@ -64,130 +26,26 @@ function CategoryPage() {
     endDateTime,
   });
 
-  useLayoutEffect(() => {
-    if (!(dateCellRef.current instanceof HTMLDivElement)) return;
-
-    setMaxScheduleCount(
-      Math.floor(
-        (Math.floor(dateCellRef.current.clientHeight / 10) * 10 - SCHEDULE.HEIGHT * rootFontSize) /
-          (SCHEDULE.HEIGHT_WITH_MARGIN * rootFontSize)
-      )
-    );
-  }, [startDateTime, rootFontSize]);
-
-  const { year: currentYear, month: currentMonth } = extractDateTime(currentDateTime);
-  const rowNum = Math.ceil(calendar.length / 7);
-
-  if (!category.id || isLoading || data === undefined) {
-    return (
-      <PageLayout type={PAGE_LAYOUT.SIDEBAR}>
-        <div css={categoryPageStyle}>
-          <CategoryControl setCategory={setCategory} />
-          <div css={calendarStyle}>
-            {!category.id && <div css={hintStyle}>클릭한 카테고리의 일정을 확인할 수 있어요</div>}
-            <div css={calendarHeaderStyle}>
-              {`${currentYear}년 ${currentMonth}월`}
-              <div css={waitingNavStyle}>
-                {isLoading && (
-                  <div css={spinnerStyle}>
-                    <Spinner size={4} />
-                    일정을 가져오고 있습니다.
-                  </div>
-                )}
-                <div css={monthPickerStyle}>
-                  <Button cssProp={navButtonStyle} onClick={moveToBeforeMonth}>
-                    <MdKeyboardArrowLeft />
-                    <span css={navButtonTitleStyle}>전 달</span>
-                  </Button>
-                  <Button cssProp={todayButtonStyle} onClick={moveToToday}>
-                    오늘
-                  </Button>
-                  <Button cssProp={navButtonStyle} onClick={moveToNextMonth}>
-                    <MdKeyboardArrowRight />
-                    <span css={navButtonTitleStyle}>다음 달</span>
-                  </Button>
-                </div>
-              </div>
-            </div>
-            <div css={dayBarGridStyle}>
-              {DAYS.map((day) => (
-                <span key={`day#${day}`} css={dayBarStyle(theme, day)}>
-                  {day}
-                </span>
-              ))}
-            </div>
-            <div css={calendarGridStyle(rowNum)}>
-              {calendar.map((dateTime) => {
-                return (
-                  <DateCell
-                    key={dateTime}
-                    dateTime={dateTime}
-                    currentMonth={currentMonth}
-                    dateCellRef={dateCellRef}
-                    readonly
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </PageLayout>
-    );
-  }
-
-  const { calendarWithPriority, getLongTermSchedulesWithPriority, getSingleSchedulesWithPriority } =
-    getSchedulePriority(calendar);
-
-  const schedulesWithPriority = {
-    longTermSchedulesWithPriority: getLongTermSchedulesWithPriority(data.data.longTerms),
-    allDaySchedulesWithPriority: getSingleSchedulesWithPriority(data.data.allDays),
-    fewHourSchedulesWithPriority: getSingleSchedulesWithPriority(data.data.fewHours),
-  };
-
   return (
     <PageLayout type={PAGE_LAYOUT.SIDEBAR}>
       <div css={categoryPageStyle}>
         <CategoryControl setCategory={setCategory} />
         <div css={calendarStyle}>
-          <div css={calendarHeaderStyle}>
-            {`${currentYear}년 ${currentMonth}월 \u00A0☾\u00A0 ${category.name}`}
-            <div css={monthPickerStyle}>
-              <Button cssProp={navButtonStyle} onClick={moveToBeforeMonth}>
-                <MdKeyboardArrowLeft />
-                <span css={navButtonTitleStyle}>전 달</span>
-              </Button>
-              <Button cssProp={todayButtonStyle} onClick={moveToToday}>
-                오늘
-              </Button>
-              <Button cssProp={navButtonStyle} onClick={moveToNextMonth}>
-                <MdKeyboardArrowRight />
-                <span css={navButtonTitleStyle}>다음 달</span>
-              </Button>
-            </div>
-          </div>
-          <div css={dayBarGridStyle}>
-            {DAYS.map((day) => (
-              <span key={`day#${day}`} css={dayBarStyle(theme, day)}>
-                {day}
-              </span>
-            ))}
-          </div>
-          <div css={calendarGridStyle(rowNum)}>
-            {calendar.map((dateTime) => {
-              return (
-                <DateCell
-                  key={dateTime}
-                  dateTime={dateTime}
-                  currentMonth={currentMonth}
-                  dateCellRef={dateCellRef}
-                  maxScheduleCount={maxScheduleCount}
-                  calendarWithPriority={calendarWithPriority}
-                  schedulesWithPriority={schedulesWithPriority}
-                  readonly
-                />
-              );
-            })}
-          </div>
+          {(isLoading || data === undefined) && (
+            <CalendarFallback
+              calendarController={calendarController}
+              category={category}
+              readonly
+            />
+          )}
+          {data && (
+            <Calendar
+              calendarController={calendarController}
+              scheduleResponse={data}
+              category={category}
+              readonly
+            />
+          )}
         </div>
       </div>
     </PageLayout>
