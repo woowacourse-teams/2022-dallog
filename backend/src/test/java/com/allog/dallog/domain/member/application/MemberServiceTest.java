b8f21adcafbfdfc40fb6c2f79ffeaa4e4b94dbf8
koo <gudonghee2000@gmail.com>
2022-11-06 04:12:33 +0900

    test: MemberServiceTest의 테스트 코드 문장형 테스트로 변경

--- backend/src/test/java/com/allog/dallog/domain/member/application/MemberServiceTest.java
+++ backend/src/test/java/com/allog/dallog/domain/member/application/MemberServiceTest.java
@@ -1,42 +1,43 @@
 package com.allog.dallog.domain.member.application;
 
-import static com.allog.dallog.common.fixtures.CategoryFixtures.BE_일정;
-import static com.allog.dallog.common.fixtures.CategoryFixtures.공통_일정_생성_요청;
-import static com.allog.dallog.common.fixtures.MemberFixtures.관리자;
-import static com.allog.dallog.common.fixtures.MemberFixtures.관리자_이름;
-import static com.allog.dallog.common.fixtures.MemberFixtures.리버;
-import static com.allog.dallog.common.fixtures.MemberFixtures.리버_이름;
-import static com.allog.dallog.common.fixtures.MemberFixtures.매트;
-import static com.allog.dallog.common.fixtures.MemberFixtures.파랑;
-import static com.allog.dallog.common.fixtures.MemberFixtures.후디;
-import static com.allog.dallog.common.fixtures.MemberFixtures.후디_이름;
-import static com.allog.dallog.common.fixtures.OAuthFixtures.매트;
-import static com.allog.dallog.common.fixtures.OAuthFixtures.파랑;
+import static com.allog.dallog.common.Constants.네오_이름;
+import static com.allog.dallog.common.Constants.네오_이메일;
+import static com.allog.dallog.common.Constants.네오_프로필_URL;
+import static com.allog.dallog.common.Constants.취업_카테고리_이름;
+import static com.allog.dallog.common.Constants.포비_이름;
+import static com.allog.dallog.common.Constants.포비_이메일;
+import static com.allog.dallog.common.Constants.포비_프로필_URL;
+import static com.allog.dallog.domain.category.domain.CategoryType.NORMAL;
+import static com.allog.dallog.domain.categoryrole.domain.CategoryRoleType.ADMIN;
+import static com.allog.dallog.domain.categoryrole.domain.CategoryRoleType.NONE;
+import static com.allog.dallog.domain.subscription.domain.Color.COLOR_1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.allog.dallog.common.annotation.ServiceTest;
-import com.allog.dallog.common.fixtures.SubscriptionFixtures;
-import com.allog.dallog.domain.category.application.CategoryService;
 import com.allog.dallog.domain.category.domain.Category;
 import com.allog.dallog.domain.category.domain.CategoryRepository;
-import com.allog.dallog.domain.category.dto.response.CategoryResponse;
+import com.allog.dallog.domain.category.domain.CategoryType;
+import com.allog.dallog.domain.categoryrole.domain.CategoryRole;
+import com.allog.dallog.domain.categoryrole.domain.CategoryRoleRepository;
 import com.allog.dallog.domain.categoryrole.exception.NoCategoryAuthorityException;
 import com.allog.dallog.domain.member.domain.Member;
 import com.allog.dallog.domain.member.domain.MemberRepository;
+import com.allog.dallog.domain.member.domain.SocialType;
 import com.allog.dallog.domain.member.dto.request.MemberUpdateRequest;
 import com.allog.dallog.domain.member.dto.response.MemberResponse;
 import com.allog.dallog.domain.member.dto.response.SubscribersResponse;
-import com.allog.dallog.domain.subscription.application.SubscriptionService;
 import com.allog.dallog.domain.subscription.domain.Subscription;
 import com.allog.dallog.domain.subscription.domain.SubscriptionRepository;
-import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class MemberServiceTest extends ServiceTest {
+
+    private final MemberUpdateRequest 네오_이름_수정_요청 = new MemberUpdateRequest("모피어스");
 
     @Autowired
     private MemberService memberService;
@@ -45,48 +46,51 @@ class MemberServiceTest extends ServiceTest {
     private MemberRepository memberRepository;
 
     @Autowired
-    private SubscriptionRepository subscriptionRepository;
-
-    @Autowired
-    private SubscriptionService subscriptionService;
-
-    @Autowired
     private CategoryRepository categoryRepository;
 
     @Autowired
-    private CategoryService categoryService;
+    private CategoryRoleRepository categoryRoleRepository;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    private User 네오;
+    private User 포비;
+
+    @BeforeEach
+    void setUp() {
+        네오 = new User();
+        포비 = new User();
+    }
 
     @DisplayName("id를 통해 회원을 단건 조회한다.")
     @Test
     void id를_통해_회원을_단건_조회한다() {
         // given
-        Long 파랑_id = toMemberId(파랑.OAuth_인증을_한다());
+        네오.회원_가입을_한다(네오_이메일, 네오_이름, 네오_프로필_URL);
 
         // when & then
-        assertThat(memberService.findById(파랑_id).getId())
-                .isEqualTo(파랑_id);
+        assertThat(memberService.findById(네오.계정().getId()).getId())
+                .isEqualTo(네오.계정().getId());
     }
 
     @DisplayName("구독 id를 기반으로 member 정보를 조회한다.")
     @Test
     void 구독_id를_기반으로_member_정보를_조회한다() {
         // given
-        Long 매트_id = toMemberId(매트.OAuth_인증을_한다());
-        Member 매트 = memberRepository.getById(매트_id);
-
-        Category BE_일정 = categoryRepository.save(BE_일정(매트));
-        Subscription 색상_1_BE_일정_구독 = subscriptionRepository.save(SubscriptionFixtures.색상1_구독(매트, BE_일정));
+        네오.회원_가입을_한다(네오_이메일, 네오_이름, 네오_프로필_URL)
+                .카테고리를_등록한다(취업_카테고리_이름, NORMAL);
 
         // when
-        MemberResponse actual = memberService.findBySubscriptionId(색상_1_BE_일정_구독.getId());
+        MemberResponse actual = memberService.findBySubscriptionId(네오.구독().getId());
 
         // then
         assertAll(() -> {
-            assertThat(actual.getId()).isEqualTo(매트.getId());
-            assertThat(actual.getEmail()).isEqualTo(매트.getEmail());
-            assertThat(actual.getDisplayName()).isEqualTo(매트.getDisplayName());
-            assertThat(actual.getProfileImageUrl()).isEqualTo(매트.getProfileImageUrl());
-            assertThat(actual.getSocialType()).isEqualTo(매트.getSocialType());
+            assertThat(actual.getId()).isEqualTo(네오.계정().getId());
+            assertThat(actual.getEmail()).isEqualTo(네오.계정().getEmail());
+            assertThat(actual.getDisplayName()).isEqualTo(네오.계정().getDisplayName());
+            assertThat(actual.getProfileImageUrl()).isEqualTo(네오.계정().getProfileImageUrl());
+            assertThat(actual.getSocialType()).isEqualTo(네오.계정().getSocialType());
         });
     }
 
@@ -94,59 +98,88 @@ class MemberServiceTest extends ServiceTest {
     @Test
     void 회원의_이름을_수정한다() {
         // given
-        Long 매트_id = toMemberId(매트.OAuth_인증을_한다());
-
-        String 패트_이름 = "패트";
-        MemberUpdateRequest 매트_수정_요청 = new MemberUpdateRequest(패트_이름);
+        네오.회원_가입을_한다(네오_이메일, 네오_이름, 네오_프로필_URL);
 
         // when
-        memberService.update(매트_id, 매트_수정_요청);
+        memberService.update(네오.계정().getId(), 네오_이름_수정_요청);
 
         // then
-        MemberResponse actual = memberService.findById(매트_id);
-        assertThat(actual.getDisplayName()).isEqualTo(패트_이름);
+        Member actual = memberRepository.getById(네오.계정().getId());
+        assertThat(actual.getDisplayName()).isEqualTo("모피어스");
     }
 
     @DisplayName("특정 카테고리의 구독자 목록을 반환한다.")
     @Test
     void 특정_카테고리의_구독자_목록을_반환한다() {
         // given
-        Member 관리자 = memberRepository.save(관리자());
-        Member 후디 = memberRepository.save(후디());
-        Member 리버 = memberRepository.save(리버());
-        memberRepository.save(파랑());
-        memberRepository.save(매트());
+        네오.회원_가입을_한다(네오_이메일, 네오_이름, 네오_프로필_URL)
+                .카테고리를_등록한다(취업_카테고리_이름, NORMAL);
 
-        CategoryResponse 카테고리 = categoryService.save(관리자.getId(), 공통_일정_생성_요청);
-
-        subscriptionService.save(후디.getId(), 카테고리.getId());
-        subscriptionService.save(리버.getId(), 카테고리.getId());
+        포비.회원_가입을_한다(포비_이메일, 포비_이름, 포비_프로필_URL)
+                .카테고리를_구독한다(네오.카테고리());
 
         // when
-        SubscribersResponse actual = memberService.findSubscribers(관리자.getId(), 카테고리.getId());
+        SubscribersResponse actual = memberService.findSubscribers(네오.계정().getId(), 네오.카테고리().getId());
 
         // then
-        assertAll(() -> {
-            assertThat(actual.getSubscribers().size()).isEqualTo(3);
-            assertThat(
-                    actual.getSubscribers().stream().map(it -> it.getMember().getDisplayName())
-                            .collect(Collectors.toList()))
-                    .containsExactly(관리자_이름, 후디_이름, 리버_이름);
-        });
+        assertThat(actual.getSubscribers().size()).isEqualTo(2);
     }
 
     @DisplayName("특정 카테고리의 구독자 목록을 ADMIN이 아닌 회원이 호출하면 예외가 발생한다.")
     @Test
     void 특정_카테고리의_구독자_목록을_ADMIN이_아닌_회원이_호출하면_예외가_발생한다() {
         // given
-        Member 관리자 = memberRepository.save(관리자());
-        Member 후디 = memberRepository.save(후디());
+        네오.회원_가입을_한다(네오_이메일, 네오_이름, 네오_프로필_URL)
+                .카테고리를_등록한다(취업_카테고리_이름, NORMAL);
 
-        CategoryResponse 카테고리 = categoryService.save(관리자.getId(), 공통_일정_생성_요청);
-        subscriptionService.save(후디.getId(), 카테고리.getId());
+        포비.회원_가입을_한다(포비_이메일, 포비_이름, 포비_프로필_URL)
+                .카테고리를_구독한다(네오.카테고리());
 
         // when & then
-        assertThatThrownBy(() -> memberService.findSubscribers(후디.getId(), 카테고리.getId()))
+        assertThatThrownBy(() -> memberService.findSubscribers(포비.계정().getId(), 네오.카테고리().getId()))
                 .isInstanceOf(NoCategoryAuthorityException.class);
+    }
+
+    private final class User {
+
+        private Member member;
+        private Category category;
+        private Subscription subscription;
+
+        public User 회원_가입을_한다(final String email, final String name, final String profile) {
+            this.member = new Member(email, name, profile, SocialType.GOOGLE);
+            memberRepository.save(member);
+            return this;
+        }
+
+        public User 카테고리를_등록한다(final String categoryName, final CategoryType categoryType) {
+            this.category = new Category(categoryName, this.member, categoryType);
+            CategoryRole categoryRole = new CategoryRole(category, this.member, ADMIN);
+            this.subscription = new Subscription(this.member, category, COLOR_1);
+            categoryRepository.save(category);
+            categoryRoleRepository.save(categoryRole);
+            subscriptionRepository.save(subscription);
+            return this;
+        }
+
+        public User 카테고리를_구독한다(final Category category) {
+            CategoryRole categoryRole = new CategoryRole(category, this.member, NONE);
+            Subscription subscription = new Subscription(this.member, category, COLOR_1);
+            categoryRoleRepository.save(categoryRole);
+            subscriptionRepository.save(subscription);
+            return this;
+        }
+
+        public Member 계정() {
+            return member;
+        }
+
+        public Category 카테고리() {
+            return category;
+        }
+
+        public Subscription 구독() {
+            return subscription;
+        }
     }
 }
