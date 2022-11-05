27e62f69c8061454d818a84f8623899ab1128194
koo <gudonghee2000@gmail.com>
2022-11-06 03:22:26 +0900

    test: ExternalCategoryDetailServiceTest 테스트 코드에 문장형 테스트 적용

--- backend/src/test/java/com/allog/dallog/domain/category/application/ExternalCategoryDetailServiceTest.java
+++ backend/src/test/java/com/allog/dallog/domain/category/application/ExternalCategoryDetailServiceTest.java
@@ -1,17 +1,24 @@
 package com.allog.dallog.domain.category.application;
 
-import static com.allog.dallog.common.fixtures.CategoryFixtures.외부_BE_일정_생성_요청;
-import static com.allog.dallog.common.fixtures.CategoryFixtures.외부_FE_일정_생성_요청;
-import static com.allog.dallog.common.fixtures.OAuthFixtures.리버;
+import static com.allog.dallog.common.Constants.네오_이름;
+import static com.allog.dallog.common.Constants.네오_이메일;
+import static com.allog.dallog.common.Constants.네오_프로필_URL;
+import static com.allog.dallog.common.Constants.외부_카테고리_ID;
+import static com.allog.dallog.common.Constants.외부_카테고리_이름;
+import static com.allog.dallog.domain.category.domain.CategoryType.GOOGLE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.allog.dallog.common.annotation.ServiceTest;
 import com.allog.dallog.domain.category.domain.Category;
 import com.allog.dallog.domain.category.domain.CategoryRepository;
+import com.allog.dallog.domain.category.domain.CategoryType;
 import com.allog.dallog.domain.category.domain.ExternalCategoryDetail;
 import com.allog.dallog.domain.category.domain.ExternalCategoryDetailRepository;
-import com.allog.dallog.domain.category.dto.response.CategoryResponse;
+import com.allog.dallog.domain.member.domain.Member;
+import com.allog.dallog.domain.member.domain.MemberRepository;
+import com.allog.dallog.domain.member.domain.SocialType;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +29,7 @@ class ExternalCategoryDetailServiceTest extends ServiceTest {
     private ExternalCategoryDetailService externalCategoryDetailService;
 
     @Autowired
-    private CategoryService categoryService;
+    private MemberRepository memberRepository;
 
     @Autowired
     private ExternalCategoryDetailRepository externalCategoryDetailRepository;
@@ -30,25 +37,47 @@ class ExternalCategoryDetailServiceTest extends ServiceTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    private User 회원;
+
+    @BeforeEach
+    void setUp() {
+        회원 = new User();
+    }
+
     @DisplayName("월별 일정 조회 시, 회원 ID로 해당하는 외부 연동 카테고리 전체를 조회한다.")
     @Test
     void 월별_일정_조회_시_회원_ID로_해당하는_외부_연동_카테고리의_전체를_조회한다() {
         // given
-        Long 리버_id = toMemberId(리버.OAuth_인증을_한다());
-
-        CategoryResponse 외부_BE_일정_응답 = categoryService.save(리버_id, 외부_BE_일정_생성_요청);
-        Category 외부_BE_일정 = categoryRepository.getById(외부_BE_일정_응답.getId());
-
-        CategoryResponse 외부_FE_일정_응답 = categoryService.save(리버_id, 외부_FE_일정_생성_요청);
-        Category 외부_FE_일정 = categoryRepository.getById(외부_FE_일정_응답.getId());
-
-        externalCategoryDetailRepository.save(new ExternalCategoryDetail(외부_BE_일정, "1111111"));
-        externalCategoryDetailRepository.save(new ExternalCategoryDetail(외부_FE_일정, "2222222"));
+        회원.회원_가입을_한다(네오_이메일, 네오_이름, 네오_프로필_URL)
+                .외부_카테고리를_등록한다(외부_카테고리_이름, GOOGLE);
 
         // when
-        List<ExternalCategoryDetail> details = externalCategoryDetailService.findByMemberId(리버_id);
+        List<ExternalCategoryDetail> actual = externalCategoryDetailService.findByMemberId(회원.계정().getId());
 
         // then
-        assertThat(details).hasSize(2);
+        assertThat(actual).hasSize(1);
+    }
+
+    private final class User {
+
+        private Member member;
+
+        public User 회원_가입을_한다(final String email, final String name, final String profile) {
+            this.member = new Member(email, name, profile, SocialType.GOOGLE);
+            memberRepository.save(member);
+            return this;
+        }
+
+        public User 외부_카테고리를_등록한다(final String categoryName, final CategoryType categoryType) {
+            Category category = new Category(categoryName, this.member, categoryType);
+            ExternalCategoryDetail externalCategoryDetail = new ExternalCategoryDetail(category, 외부_카테고리_ID);
+            categoryRepository.save(category);
+            externalCategoryDetailRepository.save(externalCategoryDetail);
+            return this;
+        }
+
+        public Member 계정() {
+            return member;
+        }
     }
 }
