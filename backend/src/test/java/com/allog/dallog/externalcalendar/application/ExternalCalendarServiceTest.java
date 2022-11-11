ac44ab309b18543688fd328d10b372f047d857de
koo <gudonghee2000@gmail.com>
2022-11-11 22:13:50 +0900

    refactor: ExternalCalendarServiceTest의 모든 테스트메서드에 문장형 테스트 적용

--- backend/src/test/java/com/allog/dallog/externalcalendar/application/ExternalCalendarServiceTest.java
+++ backend/src/test/java/com/allog/dallog/externalcalendar/application/ExternalCalendarServiceTest.java
@@ -1,19 +1,25 @@
-package com.allog.dallog.externalcalendar.application;
+package com.allog.dallog.domain.externalcalendar.application;
 
+import static com.allog.dallog.common.Constants.나인_이름;
+import static com.allog.dallog.common.Constants.나인_이메일;
+import static com.allog.dallog.common.Constants.나인_프로필_URL;
 import static com.allog.dallog.common.fixtures.OAuthTokenFixtures.OAUTH_TOKEN;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.allog.dallog.auth.domain.OAuthTokenRepository;
 import com.allog.dallog.common.annotation.ServiceTest;
-import com.allog.dallog.common.fixtures.MemberFixtures;
-import com.allog.dallog.externalcalendar.dto.ExternalCalendarsResponse;
-import com.allog.dallog.member.domain.Member;
-import com.allog.dallog.member.domain.MemberRepository;
-import org.junit.jupiter.api.DisplayName;
+import com.allog.dallog.domain.auth.domain.OAuthTokenRepository;
+import com.allog.dallog.domain.externalcalendar.dto.ExternalCalendarsResponse;
+import com.allog.dallog.domain.member.domain.Member;
+import com.allog.dallog.domain.member.domain.MemberRepository;
+import com.allog.dallog.domain.member.domain.SocialType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class ExternalCalendarServiceTest extends ServiceTest {
+
+    @Autowired
+    private ExternalCalendarService externalCalendarService;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -21,20 +27,38 @@ class ExternalCalendarServiceTest extends ServiceTest {
     @Autowired
     private OAuthTokenRepository oAuthTokenRepository;
 
-    @Autowired
-    private ExternalCalendarService externalCalendarService;
+    private IntegrationLogicBuilder 나인;
 
-    @DisplayName("member id를 활용하여 외부 캘린더 리스트를 조회한다.")
+    @BeforeEach
+    void setUp() {
+        나인 = new IntegrationLogicBuilder();
+    }
+
     @Test
-    void member_id를_활용하여_외부_캘린더_리스트를_조회한다() {
+    void 회원의_외부_캘린더_목록을_조회한다() {
         // given
-        Member 매트 = memberRepository.save(MemberFixtures.매트());
-        oAuthTokenRepository.save(OAUTH_TOKEN(매트));
+        나인.회원_가입을_한다(나인_이메일, 나인_이름, 나인_프로필_URL);
 
         // when
-        ExternalCalendarsResponse actual = externalCalendarService.findByMemberId(매트.getId());
+        ExternalCalendarsResponse actual = externalCalendarService.findByMemberId(나인.회원().getId());
 
         // then
         assertThat(actual.getExternalCalendars()).hasSize(3);
+    }
+
+    private final class IntegrationLogicBuilder {
+
+        private Member member;
+
+        private IntegrationLogicBuilder 회원_가입을_한다(final String email, final String name, final String profile) {
+            this.member = new Member(email, name, profile, SocialType.GOOGLE);
+            memberRepository.save(member);
+            oAuthTokenRepository.save(OAUTH_TOKEN(member));
+            return this;
+        }
+
+        private Member 회원() {
+            return member;
+        }
     }
 }
