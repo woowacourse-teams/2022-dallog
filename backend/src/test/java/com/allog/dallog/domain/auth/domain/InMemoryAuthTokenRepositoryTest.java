98d6a4f08724b2fccf4959788d775570b871c195
koo <gudonghee2000@gmail.com>
2022-11-12 02:51:37 +0900

    refactor: 테스트클래스의 불필요한 import 제거

--- backend/src/test/java/com/allog/dallog/domain/auth/domain/InMemoryAuthTokenRepositoryTest.java
+++ backend/src/test/java/com/allog/dallog/domain/auth/domain/InMemoryAuthTokenRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.allog.dallog.domain.auth.exception.NoSuchTokenException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
